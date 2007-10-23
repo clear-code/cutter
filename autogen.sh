383320f426a126f4f51121a3eb24cbe1010c0159
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-10-23 23:03:45 +0000

    2007-10-24  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* autogen.sh: Brand new.
    	* configure.ac: Requirement of glib.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@5 10a583ed-1e3d-0410-b285-c80daaaae891

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-set -x
-aclocal -I config \
-  && libtoolize --force --copy \
-  && autoheader \
-  && automake --add-missing --foreign --copy \
-  && autoconf
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+run()
+{
+    $@
+    if test $? -ne 0; then
+	echo "Failed $@"
+	exit 1
+    fi
+}
+
+run libtoolize --copy --force
+run aclocal
+run autoheader
+run automake --add-missing --foreign --copy
+run autoconf
