e801048a3f278ada1e410f7078a5e7a4f1907c9f
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-10-31 03:50:34 +0000

    cosmetic.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@8 10a583ed-1e3d-0410-b285-c80daaaae891

--- cutter/compile.c
+++ cutter/compile.c
@@ -32,61 +32,62 @@
  * search for test suites and add them to the worl
  */
 
-void RecurseDir(utest_world* world, const char* path, int depth)
+void
+RecurseDir(utest_world* world, const char* path, int depth)
 {
-	DIR* currdir;
-	struct dirent* dent;
-	int ret;
+    DIR* currdir;
+    struct dirent* dent;
+    int ret;
 
-	currdir = opendir(path);
-	if (!currdir) 
-	{
-		ShowError (
-				"ERROR: opendir(%s) failed to open search directory: %s\n", 
-				path, strerror (errno));
-	}
-	uassert (currdir);
+    currdir = opendir(path);
+    if (!currdir) {
+        ShowError(
+                  "ERROR: opendir(%s) failed to open search directory: %s\n", 
+                  path, strerror (errno));
+    }
+    uassert(currdir);
 
-	for (dent = readdir(currdir); dent !=NULL; dent = readdir(currdir))
-	{
-		int fnm;
-		size_t flen = strlen(path) + strlen(dent->d_name)+2;
-		char* fpath = alloca(flen);
-		uassert (fpath);
+    for (dent = readdir(currdir); dent !=NULL; dent = readdir(currdir))
+    {
+        int fnm;
+        size_t flen = strlen(path) + strlen(dent->d_name)+2;
+        char* fpath = alloca(flen);
+        uassert(fpath);
 
-		snprintf(fpath,flen,"%s/%s", path, dent->d_name);
-		fnm = fnmatch("libtest_*.so",dent->d_name,0);
-		uassert (fnm == 0 || fnm == FNM_NOMATCH);
-		if (fnm == 0)
-		{/* add to world */
-			AddSuite (world, fpath, path);
-		}
-		if (dent->d_name[0] != '.')
-		{
-			struct stat sbuf;
-			int ret;
+        snprintf(fpath,flen,"%s/%s", path, dent->d_name);
+        fnm = fnmatch("libtest_*.so",dent->d_name,0);
+        uassert(fnm == 0 || fnm == FNM_NOMATCH);
+        if (fnm == 0) {/* add to world */
+            AddSuite(world, fpath, path);
+        }
+        if (dent->d_name[0] != '.') {
+            struct stat sbuf;
+            int ret;
 
-			ret = stat(fpath, &sbuf);
-			if (ret != 0)
-			{
-				ShowError (
-						"ERROR: stat(%s,...) failed during suite search: %s\n", 
-						fpath, strerror (errno));
-				if (errno == ENOENT) continue;
-			}
-			uassert (ret == 0);
+            ret = stat(fpath, &sbuf);
+            if (ret != 0) {
+                ShowError(
+                          "ERROR: stat(%s,...) failed during suite search: %s\n", 
+                          fpath, strerror (errno));
+                if (errno == ENOENT) continue;
+            }
+            uassert(ret == 0);
 
-			if (S_ISDIR(sbuf.st_mode))
-				RecurseDir(world, fpath, depth+1);
-		}
-	}
+            if (S_ISDIR(sbuf.st_mode))
+                RecurseDir(world, fpath, depth+1);
+        }
+    }
 
-	ret = closedir(currdir);
-	uassert (ret == 0);
+    ret = closedir(currdir);
+    uassert(ret == 0);
 }
 
 
-void CompileWorld (utest_world* world)
+void
+CompileWorld (utest_world* world)
 {
-	RecurseDir(world, world->rootDir?world->rootDir:".", 0);
+    RecurseDir(world, world->rootDir?world->rootDir:".", 0);
 }
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
