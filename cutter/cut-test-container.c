6fbe52d4db6bd05987df84ad7039e3d7af7ac0d1
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-10-31 05:52:17 +0000

    2007-10-31  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* cutter/cut-test-container.[c|h]: Add skelton of GObject.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@25 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ cutter/cut-test-container.c
@@ -0,0 +1,115 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2007  Kouhei Sutou <kou@cozmixng.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+
+#include "cut-test-container.h"
+
+#define CUT_TEST_CONTAINER_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), CUT_TYPE_TEST_CONTAINER, CutTestContainerPrivate))
+
+typedef struct _CutTestContainerPrivate	CutTestContainerPrivate;
+struct _CutTestContainerPrivate
+{
+};
+
+enum
+{
+    PROP_0
+};
+
+G_DEFINE_ABSTRACT_TYPE (CutTestContainer, cut_test_container, G_TYPE_OBJECT)
+
+static void dispose        (GObject         *object);
+static void set_property   (GObject         *object,
+                            guint            prop_id,
+                            const GValue    *value,
+                            GParamSpec      *pspec);
+static void get_property   (GObject         *object,
+                            guint            prop_id,
+                            GValue          *value,
+                            GParamSpec      *pspec);
+
+static void
+cut_test_container_class_init (CutTestContainerClass *klass)
+{
+    GObjectClass *gobject_class;
+    GParamSpec *spec;
+
+    gobject_class = G_OBJECT_CLASS (klass);
+
+    gobject_class->dispose      = dispose;
+    gobject_class->set_property = set_property;
+    gobject_class->get_property = get_property;
+
+    g_type_class_add_private (gobject_class, sizeof (CutTestContainerPrivate));
+}
+
+static void
+cut_test_container_init (CutTestContainer *chr)
+{
+}
+
+CutTestContainer*
+cut_test_container_new (void)
+{
+    return g_object_new(CUT_TYPE_TEST_CONTAINER, NULL);
+}
+
+static void
+dispose (GObject *object)
+{
+    G_OBJECT_CLASS (cut_test_container_parent_class)->dispose (object);
+}
+
+static void
+set_property (GObject      *object,
+              guint         prop_id,
+              const GValue *value,
+              GParamSpec   *pspec)
+{
+    switch (prop_id) {
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+        break;
+    }
+}
+
+static void
+get_property (GObject    *object,
+              guint       prop_id,
+              GValue     *value,
+              GParamSpec *pspec)
+{
+    switch (prop_id) {
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+        break;
+    }
+}
+/*
+vi:ts=4:nowrap:ai:expandtab:sw
+*/
