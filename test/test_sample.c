c83653aaf4347a631e14d4b32bbbdded0f79fe98
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-01 05:35:23 +0000

    2007-11-01  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_sample.c: Not static.
    
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@85 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test_sample.c
+++ test/test_sample.c
@@ -9,15 +9,15 @@ test_function1 (void)
 static void
 test_function2 (void)
 {
-
+    g_warning("hoge");
 }
 
-static CutTestStruct cut_tests[] =
+CutTestStruct cut_tests[] =
 {
     {"test1", test_function1},
     {"test2", test_function2}
 };
-static const gint cut_tests_len = G_N_ELEMENTS(cut_tests);
+const gint cut_tests_len = G_N_ELEMENTS(cut_tests);
 
 /*
 vi:ts=4:nowrap:ai:expandtab:sw=4
