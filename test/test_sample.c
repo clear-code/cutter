85924aeb576759763920d694193b265be44199a9
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-01 03:09:17 +0000

    update.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@70 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test_sample.c
+++ test/test_sample.c
@@ -11,12 +11,12 @@ test_function2 (void)
 
 }
 
-static CutTestStruct tests[] =
+static CutTestStruct cut_tests[] =
 {
     {"test1", test_function1},
     {"test2", test_function2}
 };
-static const gint tests_len = G_N_ELEMENTS(tests);
+static const gint cut_tests_len = G_N_ELEMENTS(tests);
 
 /*
 vi:ts=4:nowrap:ai:expandtab:sw=4
