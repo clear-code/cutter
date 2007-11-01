fe8776269387a774df7e5c883cea562059b15d61
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-01 08:53:27 +0000

    2007-11-01  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_cut_assertion.c: Tests for cut_assertionXX().
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@95 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/test_cut_assertion.c
@@ -0,0 +1,20 @@
+#include "cut-assertion.h"
+
+static void
+test_equal_int (void)
+{
+    cut_assert_equal_int(1, 1);
+    cut_assert_equal_int(-1, -1);
+    cut_assert_equal_int(0, 0);
+}
+
+CutTestEntry cut_tests[] =
+{
+    {"euqal int test", test_equal_int},
+};
+const gint cut_tests_len = G_N_ELEMENTS(cut_tests);
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
+
