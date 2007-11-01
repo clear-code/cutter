5e75872107bf606dbb8fb8aa6903e569e52ee8c8
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-01 22:13:25 +0000

    2007-11-02  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_cut_assertion.c: Added string test.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@113 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test_cut_assertion.c
+++ test/test_cut_assertion.c
@@ -9,9 +9,17 @@ test_equal_int (void)
     cut_assert_equal_int(0, 0);
 }
 
+static void
+test_equal_string (void)
+{
+    cut_assert_equal_string("", "");
+    cut_assert_equal_string("a", "a");
+}
+
 CutTestEntry cut_tests[] =
 {
     {"euqal int test", test_equal_int},
+    {"euqal string test", test_equal_string},
 };
 const gint cut_tests_len = G_N_ELEMENTS(cut_tests);
 
