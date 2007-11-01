8fc29f1ae05ccab08ad1541d75a8544d70575e9e
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-01 03:33:16 +0000

    2007-11-01  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* cutter/cut-test-case.[c|h]: New object.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@72 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ cutter/cut-test-case.h
@@ -0,0 +1,61 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2007  Kouhei Sutou <kou@cozmixng.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef __CUT_TEST_CASE_H__
+#define __CUT_TEST_CASE_H__
+
+#include <glib-object.h>
+
+#include "cut-test-container.h"
+
+G_BEGIN_DECLS
+
+#define CUT_TYPE_TEST_CASE            (cut_test_case_get_type ())
+#define CUT_TEST_CASE(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), CUT_TYPE_TEST_CASE, CutTestCase))
+#define CUT_TEST_CASE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), CUT_TYPE_TEST_CASE, CutTestCaseClass))
+#define CUT_IS_TEST_CASE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CUT_TYPE_TEST_CASE))
+#define CUT_IS_TEST_CASE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), CUT_TYPE_TEST_CASE))
+#define CUT_TEST_CASE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), CUT_TYPE_TEST_CASE, CutTestCaseClass))
+
+typedef struct _CutTestCase      CutTestCase;
+typedef struct _CutTestCaseClass CutTestCaseClass;
+
+struct _CutTestCase
+{
+    CutTestContainer object;
+};
+
+struct _CutTestCaseClass
+{
+    CutTestContainerClass parent_class;
+};
+
+GType           cut_test_case_get_type  (void) G_GNUC_CONST;
+
+CutTestCase    *cut_test_case_new       (void);
+
+G_END_DECLS
+
+#endif /* __CUT_TEST_CASE_H__ */
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
