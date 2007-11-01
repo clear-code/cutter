81afb4506343e77842ecfdc3457e8005c7dd4dad
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-01 05:02:35 +0000

    2007-11-01  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* cutter/Makefile.am, cutter/cut-main.c: Make cutter2. cutter will be
    	replaced bt cutter2.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@80 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ cutter/cut-main.c
@@ -0,0 +1,40 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2007  Kouhei Sutou <kou@cozmixng.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+
+#include "cut-test-suite.h"
+
+int
+main (int argc, char* argv[])
+{
+    exit(0);
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
