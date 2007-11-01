fa086a6f00d1b08cfa83df165c998c7a473d9373
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-01 08:55:11 +0000

    2007-11-01  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_cut_test.c: Added name test.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@96 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test_cut_test.c
+++ test/test_cut_test.c
@@ -26,12 +26,19 @@ tear_down (void)
 static void
 test_get_assertion_count (void)
 {
-    cut_assert_equal_int(3, test_object_get_assertion_count(test_object));
+    cut_assert_equal_int(3, cut_test_get_assertion_count(test_object));
+}
+
+static void
+test_name (void)
+{
+    cut_assert_equal_string("test name test", cut_test_get_name(test_object));
 }
 
 CutTestEntry cut_tests[] =
 {
     {"assertion count test", test_assertion_count},
+    {"test name test", test_name},
 };
 const gint cut_tests_len = G_N_ELEMENTS(cut_tests);
 
