fca1f016d91604d13d7b38251daaa3f28857a2cd
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-01 03:52:46 +0000

    2007-11-01  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* cutter/cut-test-suite.[c|h]: New object.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@76 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ cutter/cut-test-suite.c
@@ -0,0 +1,117 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2007  Kouhei Sutou <kou@cozmixng.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+
+#include "cut-test-suite.h"
+
+#include "cut-test.h"
+
+#define CUT_TEST_SUITE_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), CUT_TYPE_TEST_SUITE, CutTestSuitePrivate))
+
+typedef struct _CutTestSuitePrivate	CutTestSuitePrivate;
+struct _CutTestSuitePrivate
+{
+};
+
+enum
+{
+    PROP_0
+};
+
+G_DEFINE_TYPE (CutTestSuite, cut_test_suite, CUT_TYPE_TEST_CONTAINER)
+
+static void dispose        (GObject         *object);
+static void set_property   (GObject         *object,
+                            guint            prop_id,
+                            const GValue    *value,
+                            GParamSpec      *pspec);
+static void get_property   (GObject         *object,
+                            guint            prop_id,
+                            GValue          *value,
+                            GParamSpec      *pspec);
+
+static void
+cut_test_suite_class_init (CutTestSuiteClass *klass)
+{
+    GObjectClass *gobject_class;
+
+    gobject_class = G_OBJECT_CLASS(klass);
+
+    gobject_class->dispose      = dispose;
+    gobject_class->set_property = set_property;
+    gobject_class->get_property = get_property;
+
+    g_type_class_add_private(gobject_class, sizeof(CutTestSuitePrivate));
+}
+
+static void
+cut_test_suite_init (CutTestSuite *test_suite)
+{
+}
+
+static void
+dispose (GObject *object)
+{
+    G_OBJECT_CLASS(cut_test_suite_parent_class)->dispose(object);
+}
+
+static void
+set_property (GObject      *object,
+              guint         prop_id,
+              const GValue *value,
+              GParamSpec   *pspec)
+{
+    switch (prop_id) {
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+        break;
+    }
+}
+
+static void
+get_property (GObject    *object,
+              guint       prop_id,
+              GValue     *value,
+              GParamSpec *pspec)
+{
+    switch (prop_id) {
+      default:
+        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+        break;
+    }
+}
+
+CutTestSuite *
+cut_test_suite_new (void)
+{
+    return g_object_new(CUT_TYPE_TEST_SUITE, NULL);
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
---
+++ cutter/cut-test-suite.h
@@ -0,0 +1,61 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2007  Kouhei Sutou <kou@cozmixng.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *  Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef __CUT_TEST_SUITE_H__
+#define __CUT_TEST_SUITE_H__
+
+#include <glib-object.h>
+
+#include "cut-test-container.h"
+
+G_BEGIN_DECLS
+
+#define CUT_TYPE_TEST_SUITE            (cut_test_suite_get_type ())
+#define CUT_TEST_SUITE(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), CUT_TYPE_TEST_SUITE, CutTestSuite))
+#define CUT_TEST_SUITE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), CUT_TYPE_TEST_SUITE, CutTestSuiteClass))
+#define CUT_IS_TEST_SUITE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CUT_TYPE_TEST_SUITE))
+#define CUT_IS_TEST_SUITE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), CUT_TYPE_TEST_SUITE))
+#define CUT_TEST_SUITE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), CUT_TYPE_TEST_SUITE, CutTestSuiteClass))
+
+typedef struct _CutTestSuite      CutTestSuite;
+typedef struct _CutTestSuiteClass CutTestSuiteClass;
+
+struct _CutTestSuite
+{
+    CutTestContainer object;
+};
+
+struct _CutTestSuiteClass
+{
+    CutTestContainerClass parent_class;
+};
+
+GType           cut_test_suite_get_type  (void) G_GNUC_CONST;
+
+CutTestSuite    *cut_test_suite_new       (void);
+
+G_END_DECLS
+
+#endif /* __CUT_TEST_SUITE_H__ */
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
