74b778618f23c732b98505311a834458dc1c99c7
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-04 23:18:11 +0000

    2007-11-05  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_cut_loader.c: Added CutLoader test.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@187 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/test_cut_loader.c
@@ -0,0 +1,33 @@
+#include "cut.h"
+#include "cut-loader.h"
+
+void test_load_function (void);
+
+static CutLoader *test_loader;
+
+void
+setup (void)
+{
+    test_loader = cut_loader_new("loader_test_dir/.libs");
+}
+
+void
+teardown (void)
+{
+    g_object_unref(test_loader);
+}
+
+void
+test_load_function (void)
+{
+    CutTestCase *test_case;
+
+    test_case = cut_loader_load_test_case(test_loader);
+
+    cut_assert(test_case);
+    g_object_unref(test_case);
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
