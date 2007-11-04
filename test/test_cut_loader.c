db2e4320ce8cec633cdce70272bb9ff0eca158fa
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-04 23:54:01 +0000

    2007-11-05  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_cut_loader.c: Check loaded functions.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@192 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test_cut_loader.c
+++ test/test_cut_loader.c
@@ -17,15 +17,46 @@ teardown (void)
     g_object_unref(test_loader);
 }
 
+static const gchar *expected_functions[] = {
+"test_abcdefghijklmnopqratuvwzyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789",
+"test_dummy_function1",
+"test_dummy_function2",
+"test_dummy_function3"
+};
+
+static gint
+compare_function_name (gconstpointer a, gconstpointer b)
+{
+    CutTest *ta, *tb;
+    ta = CUT_TEST(a);
+    tb = CUT_TEST(b);
+
+    return strcmp(cut_test_get_function_name(ta), cut_test_get_function_name(tb));
+}
+
 void
 test_load_function (void)
 {
     CutTestCase *test_case;
+    GList *tests, *list;
+    gint i;
 
     test_case = cut_loader_load_test_case(test_loader);
 
     cut_assert(test_case);
     cut_assert_equal_int(4, cut_test_case_get_test_count(test_case));
+    tests = (GList *) cut_test_container_get_children(CUT_TEST_CONTAINER(test_case));
+    cut_assert(tests);
+    tests = g_list_sort(tests, compare_function_name);
+    for (list = tests, i = 0; list; list = g_list_next(list), i++) {
+        CutTest *test;
+
+        cut_assert(list->data);
+        cut_assert(CUT_IS_TEST(list->data));
+
+        test = CUT_TEST(list->data);
+        cut_assert_equal_string(expected_functions[i], cut_test_get_function_name(test));
+    }
 
     g_object_unref(test_case);
 }
