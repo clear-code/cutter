b67839016ae593a094676120221e058d4d73ad15
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-04 23:53:21 +0000

    cosmetic fix.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@191 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/loader_test_dir/dummy_test_functions.c
+++ test/loader_test_dir/dummy_test_functions.c
@@ -1,10 +1,11 @@
 #include "cut.h"
 #include <stdio.h>
 
+void test_abcdefghijklmnopqratuvwzyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 (void);
 void test_dummy_function1 (void);
 void test_dummy_function2 (void);
 void test_dummy_function3 (void);
-void test_abcdefghijklmnopqratuvwzyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 (void);
+
 gboolean test_invalid_return_value_function (void);
 void test_invalid_argument_type_function (gboolean arugment);
 void text_invalid_prefix_function (void);
