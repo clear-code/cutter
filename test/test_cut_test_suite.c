930737296cc53f53905b56893ed0ff05e28fe4ff
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-05 22:38:40 +0000

    2007-11-05  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_cut_test_suite.c: Added test for cut_test_suite_run_test_case().
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@257 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test_cut_test_suite.c
+++ test/test_cut_test_suite.c
@@ -1,18 +1,39 @@
 #include "cutter.h"
+#include "cut-test-case.h"
 #include "cut-test-suite.h"
+#include "cut-loader.h"
 
-static CutTestSuite *test_suite;
+void test_run_test_case (void);
+
+static CutTestSuite *test_object;
+static CutLoader *loader1, *loader2;
 
 void
 setup (void)
 {
-    test_suite = cut_test_suite_new();
+    CutTestCase *test_case;
+    test_object = cut_test_suite_new();
+
+    loader1 = cut_loader_new("loader_test_dir/.libs/libdummy_loader_test.so");
+    loader2 = cut_loader_new("loader_test_dir/.libs/libdummy_loader_test2.so");
+    test_case = cut_loader_load_test_case(loader1);
+    cut_test_container_add_test(CUT_TEST_CONTAINER(test_object), CUT_TEST(test_case));
+    test_case = cut_loader_load_test_case(loader2);
+    cut_test_container_add_test(CUT_TEST_CONTAINER(test_object), CUT_TEST(test_case));
 }
 
 void
 teardown (void)
 {
-    g_object_unref(test_suite);
+    g_object_unref(loader1);
+    g_object_unref(loader2);
+    g_object_unref(test_object);
+}
+
+void
+test_run_test_case (void)
+{
+    cut_test_suite_run_test_case (test_object, "dummy_loader_test2");
 }
 
 /*
