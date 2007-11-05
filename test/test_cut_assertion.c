977d43be59dc09f75df5f0677261344f2c69f279
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-05 01:27:38 +0000

    2007-11-05  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test-cut-assrtion.h: Added a test for cut_fail().
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@206 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test_cut_assertion.c
+++ test/test_cut_assertion.c
@@ -3,6 +3,13 @@
 void test_equal_int(void);
 void test_equal_string(void);
 void test_equal_double(void);
+void test_fail(void);
+
+static void
+dummy_fail_test_function (void)
+{
+    cut_fail("This test should fail");
+}
 
 void
 test_equal_int (void)
@@ -24,6 +31,23 @@ test_equal_double (void)
 {
     cut_assert_equal_double(1.0, 0.1, 1.0);
     cut_assert_equal_double(1.0, 0.01, 1.01);
+}
+
+void
+test_fail (void)
+{
+    CutTest *test_object;
+    CutTest *original_test;
+    gboolean ret;
+
+    test_object = cut_test_new("dummy-fail-test", dummy_fail_test_function);
+    cut_assert(test_object);
+    original_test = cut_context_get_current_test(cut_context_get_current());
+    cut_context_set_test(cut_context_get_current(), test_object);
+    ret = cut_test_run(test_object);
+    cut_context_set_test(cut_context_get_current(), original_test);
+    cut_assert(!ret);
+    g_object_unref(test_object);
 }
 
 /*
