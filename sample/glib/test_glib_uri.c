68d4ce0407d615cba7d48e398f2182e132267efd
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-05 09:47:15 +0000

    2007-11-05  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* sample/glib/test_glib_uri.c: Separate some tests.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@235 10a583ed-1e3d-0410-b285-c80daaaae891

--- sample/glib/test_glib_uri.c
+++ sample/glib/test_glib_uri.c
@@ -28,7 +28,9 @@
 #include "cut.h"
 
 void test_g_filename_to_uri (void);
+void test_g_filename_to_uri_error (void);
 void test_g_filename_from_uri (void);
+void test_g_filename_from_uri_error (void);
 void test_roundtrip (void);
 void test_uri_list (void);
 
@@ -54,22 +56,12 @@ to_uri_tests[] = {
   { "\\\\server\\share\\dir", "localhost", "file:////server/share/dir"},
 #else
   { "/etc", "localhost", "file://localhost/etc"},
-  { "c:\\windows", NULL, NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH}, /* it's important to get this error on Unix */
-  { "c:\\windows", "localhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
-  { "c:\\windows", "otherhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
 #endif
-  { "etc", "localhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
 #ifndef G_PLATFORM_WIN32
   { "/etc/\xE5\xE4\xF6", NULL, "file:///etc/%E5%E4%F6" },
   { "/etc/\xC3\xB6\xC3\xA4\xC3\xA5", NULL, "file:///etc/%C3%B6%C3%A4%C3%A5"},
 #endif
-  { "/etc", "\xC3\xB6\xC3\xA4\xC3\xA5", NULL, G_CONVERT_ERROR_ILLEGAL_SEQUENCE},
-  { "/etc", "\xE5\xE4\xF6", NULL, G_CONVERT_ERROR_ILLEGAL_SEQUENCE},
   { "/etc/file with #%", NULL, "file:///etc/file%20with%20%23%25"},
-  { "", NULL, NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
-  { "", "", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
-  { "", "localhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
-  { "", "otherhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
   { "/0123456789", NULL, "file:///0123456789"},
   { "/ABCDEFGHIJKLMNOPQRSTUVWXYZ", NULL, "file:///ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
   { "/abcdefghijklmnopqrstuvwxyz", NULL, "file:///abcdefghijklmnopqrstuvwxyz"},
@@ -87,9 +79,29 @@ to_uri_tests[] = {
    */
   { "/:", NULL, "file:///:"},
   { "/?&=", NULL, "file:///%3F&="}, 
-  { "/", "0123456789-", NULL, G_CONVERT_ERROR_ILLEGAL_SEQUENCE},
   { "/", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "file://ABCDEFGHIJKLMNOPQRSTUVWXYZ/"},
   { "/", "abcdefghijklmnopqrstuvwxyz", "file://abcdefghijklmnopqrstuvwxyz/"},
+};
+
+
+ToUriTest
+to_uri_error_tests[] = {
+#ifndef G_OS_WIN32
+  { "c:\\windows", NULL, NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH}, /* it's important to get this error on Unix */
+  { "c:\\windows", "localhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
+  { "c:\\windows", "otherhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
+#endif
+  { "etc", "localhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
+  { "/etc", "\xC3\xB6\xC3\xA4\xC3\xA5", NULL, G_CONVERT_ERROR_ILLEGAL_SEQUENCE},
+  { "/etc", "\xE5\xE4\xF6", NULL, G_CONVERT_ERROR_ILLEGAL_SEQUENCE},
+  { "", NULL, NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
+  { "", "", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
+  { "", "localhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
+  { "", "otherhost", NULL, G_CONVERT_ERROR_NOT_ABSOLUTE_PATH},
+  /* This and some of the following are of course as such illegal file names on Windows,
+   * and would not occur in real life.
+   */
+  { "/", "0123456789-", NULL, G_CONVERT_ERROR_ILLEGAL_SEQUENCE},
   { "/", "_.!~*'()", NULL, G_CONVERT_ERROR_ILLEGAL_SEQUENCE},
   { "/", "\"#%<>[\\]^`{|}\x7F", NULL, G_CONVERT_ERROR_ILLEGAL_SEQUENCE},
   { "/", ";?&=+$,", NULL, G_CONVERT_ERROR_ILLEGAL_SEQUENCE},
@@ -128,15 +140,7 @@ from_uri_tests[] = {
 #endif
   { "file://otherhost/etc", "/etc", "otherhost"},
   { "file://otherhost/etc/%23%25%20file", "/etc/#% file", "otherhost"},
-  { "file://%C3%B6%C3%A4%C3%A5/etc", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
   { "file:////etc/%C3%B6%C3%C3%C3%A5", "//etc/\xc3\xb6\xc3\xc3\xc3\xa5", NULL},
-  { "file://\xE5\xE4\xF6/etc", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
-  { "file://%E5%E4%F6/etc", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
-  { "file:///some/file#bad", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
-  { "file://some", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
-  { "", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
-  { "file:test", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
-  { "http://www.yahoo.com/", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
   { "file:////etc", "//etc"},
   { "file://///etc", "///etc"},
 #ifdef G_OS_WIN32
@@ -153,9 +157,22 @@ from_uri_tests[] = {
   { "file:///c:/foo", "/c:/foo"},
   { "file:////c:/foo", "//c:/foo"},
 #endif
-  { "file://0123456789/", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
   { "file://ABCDEFGHIJKLMNOPQRSTUVWXYZ/", "/", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
   { "file://abcdefghijklmnopqrstuvwxyz/", "/", "abcdefghijklmnopqrstuvwxyz"},
+};
+
+
+FromUriTest
+from_uri_error_tests[] = {
+  { "file://%C3%B6%C3%A4%C3%A5/etc", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
+  { "file://\xE5\xE4\xF6/etc", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
+  { "file://%E5%E4%F6/etc", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
+  { "file:///some/file#bad", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
+  { "file://some", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
+  { "", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
+  { "file:test", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
+  { "http://www.yahoo.com/", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
+  { "file://0123456789/", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
   { "file://-_.!~*'()/", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
   { "file://\"<>[\\]^`{|}\x7F/", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
   { "file://;?&=+$,/", NULL, NULL, G_CONVERT_ERROR_BAD_URI},
@@ -173,27 +190,35 @@ test_g_filename_to_uri (void)
 {
   int i;
   gchar *res;
-  GError *error;
   
   for (i = 0; i < G_N_ELEMENTS (to_uri_tests); i++)
     {
-      error = NULL;
       res = g_filename_to_uri (to_uri_tests[i].filename,
 			       to_uri_tests[i].hostname,
+			       NULL);
+      cut_assert_equal_string(to_uri_tests[i].expected_result, res);
+      g_free (res);
+    }
+}
+
+void
+test_g_filename_to_uri_error (void)
+{
+  int i;
+  gchar *res;
+  GError *error;
+  
+  for (i = 0; i < G_N_ELEMENTS (to_uri_error_tests); i++)
+    {
+      error = NULL;
+      res = g_filename_to_uri (to_uri_error_tests[i].filename,
+			       to_uri_error_tests[i].hostname,
 			       &error);
-      if (to_uri_tests[i].expected_result)
-	{
-	  cut_assert_equal_string(to_uri_tests[i].expected_result, res);
-	  g_free (res);
-	}
-      else
-      	{
-	  cut_assert (!res);
-	  cut_assert (error);
-	  cut_assert (error->domain == G_CONVERT_ERROR);
-	  cut_assert_equal_int (to_uri_tests[i].expected_error, error->code);
-	  g_error_free (error);
-	}
+      cut_assert (!res);
+      cut_assert (error);
+      cut_assert (error->domain == G_CONVERT_ERROR);
+      cut_assert_equal_int (to_uri_tests[i].expected_error, error->code);
+      g_error_free (error);
     }
 }
 
@@ -203,38 +228,47 @@ test_g_filename_from_uri (void)
   int i;
   gchar *res;
   gchar *hostname;
-  GError *error;
   
   for (i = 0; i < G_N_ELEMENTS (from_uri_tests); i++)
     {
-      error = NULL;
       res = g_filename_from_uri (from_uri_tests[i].uri,
 			         &hostname,
-			         &error);
-      if (from_uri_tests[i].expected_filename)
-	{
-	  cut_assert_equal_string (from_uri_tests[i].expected_filename, res);
-	  cut_assert_equal_string (from_uri_tests[i].expected_hostname, hostname);
-	  g_free (res);
-	}
-      else
-      	{
-#ifdef G_OS_WIN32
-	  gchar *slash, *p;
+			         NULL);
+      cut_assert_equal_string (from_uri_tests[i].expected_filename, res);
+      cut_assert_equal_string (from_uri_tests[i].expected_hostname, hostname);
+      g_free (res);
+    }
+}
 
-	  p = from_uri_tests[i].expected_filename = g_strdup (from_uri_tests[i].expected_filename);
-	  while ((slash = strchr (p, '/')) != NULL)
-	    {
-	      *slash = '\\';
-	      p = slash + 1;
-	    }
-#endif
-	  cut_assert (!res);
-	  cut_assert (error);
-	  cut_assert (error->domain == G_CONVERT_ERROR);
-	  cut_assert_equal_int (from_uri_tests[i].expected_error, error->code);
-	  g_error_free (error);
+void
+test_g_filename_from_uri_error (void)
+{
+  int i;
+  gchar *res;
+  gchar *hostname;
+  GError *error;
+  
+  for (i = 0; i < G_N_ELEMENTS (from_uri_error_tests); i++)
+    {
+      error = NULL;
+      res = g_filename_from_uri (from_uri_error_tests[i].uri,
+			         &hostname,
+			         &error);
+#ifdef G_OS_WIN32
+      gchar *slash, *p;
+
+      p = from_uri_error_tests[i].expected_filename = g_strdup (from_uri_error_tests[i].expected_filename);
+      while ((slash = strchr (p, '/')) != NULL)
+        {
+          *slash = '\\';
+	  p = slash + 1;
 	}
+#endif
+      cut_assert (!res);
+      cut_assert (error);
+      cut_assert (error->domain == G_CONVERT_ERROR);
+      cut_assert_equal_int (from_uri_error_tests[i].expected_error, error->code);
+      g_error_free (error);
     }
 }
 
@@ -247,9 +281,6 @@ test_roundtrip (void)
   
   for (i = 0; i < G_N_ELEMENTS (to_uri_tests); i++)
     {
-      if (to_uri_tests[i].expected_error != 0)
-	continue;
-
       error = NULL;
       uri = g_filename_to_uri (to_uri_tests[i].filename,
 			       to_uri_tests[i].hostname,
