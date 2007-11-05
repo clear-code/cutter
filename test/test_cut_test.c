51dcffffe74fa1287c21819d35e7396147d3b04c
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-05 23:36:29 +0000

    2007-11-05  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_cut_test.c: Added get_name test and run test.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@261 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test_cut_test.c
+++ test/test_cut_test.c
@@ -3,8 +3,11 @@
 #include "cut-context-private.h"
 
 void test_assertion_count(void);
+void test_get_function_name(void);
+void test_run(void);
 
 static CutTest *test_object;
+static gboolean run_test_flag = FALSE;
 
 static void
 dummy_test_function (void)
@@ -12,12 +15,14 @@ dummy_test_function (void)
     cut_assert_equal_int(1, 1);
     cut_assert_equal_int(1, 1);
     cut_assert_equal_int(1, 1);
+    run_test_flag = TRUE;
 }
 
 void
 setup (void)
 {
     test_object = cut_test_new("dummy-test", dummy_test_function);
+    run_test_flag = FALSE;
 }
 
 void
@@ -26,8 +31,8 @@ teardown (void)
     g_object_unref(test_object);
 }
 
-void
-test_assertion_count (void)
+static gboolean
+run_the_test (void)
 {
     CutTest *original_test;
     gboolean ret;
@@ -36,8 +41,28 @@ test_assertion_count (void)
     cut_context_set_test(cut_context_get_current(), test_object);
     ret = cut_test_run(test_object);
     cut_context_set_test(cut_context_get_current(), original_test);
-    cut_assert(ret);
+
+    return ret;
+}
+
+void
+test_assertion_count (void)
+{
+    cut_assert(run_the_test());
     cut_assert_equal_int(3, cut_test_get_assertion_count(test_object));
+}
+
+void
+test_get_function_name (void)
+{
+    cut_assert_equal_string("dummy-test", cut_test_get_function_name(test_object));
+}
+
+void
+test_run (void)
+{
+    cut_assert(run_the_test());
+    cut_assert(run_test_flag);
 }
 
 /*
