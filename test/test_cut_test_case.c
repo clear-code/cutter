43460173d9ae7541adca25808b166af3c8194b54
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-05 11:46:38 +0000

    2007-11-05  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_cut_test_case.c: Added cut_test_case_run_function() test.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@250 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test_cut_test_case.c
+++ test/test_cut_test_case.c
@@ -4,12 +4,14 @@
 void test_setup(void);
 void test_teardown(void);
 void test_test_case_count(void);
-
+void test_run_this_function(void);
 
 static CutTestCase *test_object;
 
 static gboolean setup_flag = FALSE;
 static gboolean teardown_flag = FALSE;
+static gboolean run_dummy_run_test_function_flag = FALSE;
+static gboolean run_dummy_test_function_flag = FALSE;
 
 static void
 dummy_test_function (void)
@@ -18,11 +20,20 @@ dummy_test_function (void)
     cut_assert_equal_int(1, 1);
     cut_assert_equal_int(1, 1);
     cut_assert_equal_int(1, 1);
+    run_dummy_test_function_flag = TRUE;
+}
+
+static void
+dummy_run_test_function (void)
+{
+    run_dummy_run_test_function_flag = TRUE;
 }
 
 static void
 dummy_setup_function (void)
 {
+    run_dummy_run_test_function_flag = FALSE;
+    run_dummy_test_function_flag = FALSE;
     setup_flag = TRUE;
 }
 
@@ -42,6 +53,8 @@ setup (void)
     test = cut_test_new("dummy test 1", dummy_test_function);
     cut_test_case_add_test(test_object, test);
     test = cut_test_new("dummy test 2", dummy_test_function);
+    cut_test_case_add_test(test_object, test);
+    test = cut_test_new("run_test_function", dummy_run_test_function);
     cut_test_case_add_test(test_object, test);
 }
 
@@ -71,6 +84,14 @@ void
 test_test_case_count (void)
 {
     cut_assert_equal_int(2, cut_test_case_get_test_count(test_object));
+}
+
+void
+test_run_this_function (void)
+{
+    cut_assert(cut_test_case_run_function(test_object, "run_test_function"));
+    cut_assert(run_dummy_run_test_function_flag);
+    cut_assert(!run_dummy_test_function_flag);
 }
 
 /*
