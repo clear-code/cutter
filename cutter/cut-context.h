75fa3c38e2e8b83b31f70c2e229ef3cf95d9721f
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-05 01:21:22 +0000

    2007-11-05  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* cutter/cut-context.[c|h]: Added cut_context_set_current().
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@202 10a583ed-1e3d-0410-b285-c80daaaae891

--- cutter/cut-context.h
+++ cutter/cut-context.h
@@ -71,6 +71,7 @@ void  cut_context_output_error_log          (CutContext *context);
 void  cut_context_output_normal_log         (CutContext *context);
 
 CutContext *cut_context_get_current (void);
+void  cut_context_set_current       (CutContext *context);
 
 G_END_DECLS
 
