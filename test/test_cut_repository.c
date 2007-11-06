b9fd7e7ece4c2f7f0aeb8f4be4bfb69fcc0ba2a7
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-06 01:11:10 +0000

    2007-11-06  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test_cut_repository.c: Added.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@267 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/test_cut_repository.c
@@ -0,0 +1,66 @@
+#include "cutter.h"
+#include "cut-repository.h"
+#include "cut-test-case.h"
+#include "cut-test-container.h"
+
+void test_create_test_suite (void);
+
+static CutRepository *test_repository;
+
+void
+setup (void)
+{
+    test_repository = cut_repository_new("./");
+}
+
+void
+teardown (void)
+{
+    g_object_unref(test_repository);
+}
+
+static const gchar *expected_test_case_name[] = {
+"test_case1",
+"test_case2",
+"test_case3"
+};
+
+static gint
+compare_test_case_name (gconstpointer a, gconstpointer b)
+{
+    CutTestCase *ta, *tb;
+    ta = CUT_TEST_CASE(a);
+    tb = CUT_TEST_CASE(b);
+
+    return strcmp(cut_test_case_get_name(ta), cut_test_case_get_name(tb));
+}
+
+void
+test_create_test_suite (void)
+{
+    CutTestSuite *suite;
+    GList *test_cases, *list;
+    gint i;
+
+    suite = cut_repository_create_test_suite(test_repository);
+    cut_assert(suite);
+
+    test_cases = (GList *) cut_test_container_get_children(CUT_TEST_CONTAINER(suite));
+    cut_assert(test_cases);
+
+    test_cases = g_list_sort(test_cases, compare_test_case_name);
+    for (list = test_cases, i = 0; list; list = g_list_next(list), i++) {
+        CutTestCase *test_case;
+
+        cut_assert(CUT_IS_TEST_CASE(list->data));
+        test_case = CUT_TEST_CASE(list->data);
+
+        cut_assert_equal_string(expected_test_case_name[i], cut_test_case_get_name(test_case));
+    }
+
+    g_object_unref(suite);
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
