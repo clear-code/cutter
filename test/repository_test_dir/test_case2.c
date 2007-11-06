fb43f0dfd3469d2c1ca95d4acd459b4695e22cf5
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-06 01:19:28 +0000

    2007-11-06  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/repository_test_dir: Added dir for storing test data CutTestRepository.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@270 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/repository_test_dir/test_case2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void test_dummy_function1 (void);
+void test_dummy_function2 (void);
+void test_dummy_function3 (void);
+
+void
+test_dummy_function1 (void)
+{
+}
+
+void
+test_dummy_function2 (void)
+{
+}
+
+void
+test_dummy_function3 (void)
+{
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
