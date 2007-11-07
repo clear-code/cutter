f8109b2eca1585d5a891a13cb0e733b2687bc79c
Kouhei Sutou <kou@cozmixng.org>
2007-11-07 10:07:08 +0000

    * test/loader_test_dir/dummy-test-functions.c: suppress warnings.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@410 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/loader_test_dir/dummy-test-functions.c
+++ test/loader_test_dir/dummy-test-functions.c
@@ -34,28 +34,15 @@ test_abcdefghijklmnopqratuvwzyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 (void)
 static void
 test_static_function (void)
 {
-    printf("This function is declared as static, should not be loaded!");
+    cut_error("This function is declared as static, should not be loaded!");
 }
-
-/*
-int
-test_invalid_return_value_function (void)
-{
-    printf("This test returns integer value, should not be loaded!");
-    return 1;
-}
-
-void
-test_invalid_argument_type_function (int argument)
-{
-    printf("This test has an boolean argument, should not be loaded!");
-}
-*/
 
 void
 text_invalid_prefix_function (void)
 {
-    printf("This test name does not have \"test_\" prefix, should not be loaded!");
+    cut_error("This test name does not have \"test_\" prefix,"
+              "should not be loaded!");
+    test_static_function(); /* must not called! */
 }
 
 /*
