4a66b198518f1c494053e3b2f9858c0fb723b3af
Kouhei Sutou <kou@cozmixng.org>
2007-11-08 07:56:13 +0000

    * cutter/cut-context.c: supported property to avoid warnings.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@482 10a583ed-1e3d-0410-b285-c80daaaae891

--- cutter/cut-context.c
+++ cutter/cut-context.c
@@ -47,7 +47,13 @@ struct _CutContextPrivate
 
 enum
 {
-    PROP_0
+    PROP_0,
+    PROP_N_TESTS,
+    PROP_N_ASSERTIONS,
+    PROP_N_FAILURES,
+    PROP_N_ERRORS,
+    PROP_N_PENDINGS,
+    PROP_USE_MULTI_THREAD
 };
 
 G_DEFINE_TYPE (CutContext, cut_context, G_TYPE_OBJECT)
@@ -66,12 +72,55 @@ static void
 cut_context_class_init (CutContextClass *klass)
 {
     GObjectClass *gobject_class;
+    GParamSpec *spec;
 
     gobject_class = G_OBJECT_CLASS(klass);
 
     gobject_class->dispose      = dispose;
     gobject_class->set_property = set_property;
     gobject_class->get_property = get_property;
+
+    spec = g_param_spec_uint("n-tests",
+                             "Number of tests",
+                             "The number of tests of the context",
+                             0, G_MAXUINT32, 0,
+                             G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
+    g_object_class_install_property(gobject_class, PROP_N_TESTS, spec);
+
+    spec = g_param_spec_uint("n-assertions",
+                             "Number of assertions",
+                             "The number of assertions of the context",
+                             0, G_MAXUINT32, 0,
+                             G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
+    g_object_class_install_property(gobject_class, PROP_N_ASSERTIONS, spec);
+
+    spec = g_param_spec_uint("n-failures",
+                             "Number of failures",
+                             "The number of failures of the context",
+                             0, G_MAXUINT32, 0,
+                             G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
+    g_object_class_install_property(gobject_class, PROP_N_FAILURES, spec);
+
+    spec = g_param_spec_uint("n-errors",
+                             "Number of errors",
+                             "The number of errors of the context",
+                             0, G_MAXUINT32, 0,
+                             G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
+    g_object_class_install_property(gobject_class, PROP_N_ERRORS, spec);
+
+    spec = g_param_spec_uint("n-pendings",
+                             "Number of pendings",
+                             "The number of pendings of the context",
+                             0, G_MAXUINT32, 0,
+                             G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
+    g_object_class_install_property(gobject_class, PROP_N_PENDINGS, spec);
+
+    spec = g_param_spec_boolean("use-multi-thread",
+                                "Use multi thread",
+                                "Whether use multi thread or not in the context",
+                                FALSE,
+                                G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY);
+    g_object_class_install_property(gobject_class, PROP_USE_MULTI_THREAD, spec);
 
     g_type_class_add_private(gobject_class, sizeof(CutContextPrivate));
 }
@@ -119,6 +168,24 @@ set_property (GObject      *object,
     CutContextPrivate *priv = CUT_CONTEXT_GET_PRIVATE(object);
 
     switch (prop_id) {
+      case PROP_N_TESTS:
+        priv->n_tests = g_value_get_uint(value);
+        break;
+      case PROP_N_ASSERTIONS:
+        priv->n_assertions = g_value_get_uint(value);
+        break;
+      case PROP_N_FAILURES:
+        priv->n_failures = g_value_get_uint(value);
+        break;
+      case PROP_N_ERRORS:
+        priv->n_errors = g_value_get_uint(value);
+        break;
+      case PROP_N_PENDINGS:
+        priv->n_pendings = g_value_get_uint(value);
+        break;
+      case PROP_USE_MULTI_THREAD:
+        priv->use_multi_thread = g_value_get_boolean(value);
+        break;
       default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
         break;
@@ -134,6 +201,24 @@ get_property (GObject    *object,
     CutContextPrivate *priv = CUT_CONTEXT_GET_PRIVATE(object);
 
     switch (prop_id) {
+      case PROP_N_TESTS:
+        g_value_set_uint(value, priv->n_tests);
+        break;
+      case PROP_N_ASSERTIONS:
+        g_value_set_uint(value, priv->n_assertions);
+        break;
+      case PROP_N_FAILURES:
+        g_value_set_uint(value, priv->n_failures);
+        break;
+      case PROP_N_ERRORS:
+        g_value_set_uint(value, priv->n_errors);
+        break;
+      case PROP_N_PENDINGS:
+        g_value_set_uint(value, priv->n_pendings);
+        break;
+      case PROP_USE_MULTI_THREAD:
+        g_value_set_boolean(value, priv->use_multi_thread);
+        break;
       default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
         break;
