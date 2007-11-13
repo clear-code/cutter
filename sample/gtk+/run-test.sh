c9e60501af74c6224721310d71ebef62e296c4f9
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-13 02:59:44 +0000

    2007-11-13  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* sample/gtk+/*: Added a test for GtkBuilder but not worked yet.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@545 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ sample/gtk+/run-test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export BASE_DIR="`dirname $0`"
+
+if test x"$NO_MAKE" != x"yes"; then
+    make > /dev/null || exit 1
+fi
+
+$BASE_DIR/../../cutter/cutter --color=auto \
+    -s $BASE_DIR "$@" $BASE_DIR
