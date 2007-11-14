1435c0ba9fba61975d436dd4f46ea4a7115e6a59
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-14 02:14:17 +0000

    added CUT_OUTPUT_MODULE_DIR.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@582 10a583ed-1e3d-0410-b285-c80daaaae891

--- sample/gtk+/run-test.sh
+++ sample/gtk+/run-test.sh
@@ -6,5 +6,5 @@ if test x"$NO_MAKE" != x"yes"; then
     make -C ../../ > /dev/null || exit 1
 fi
 
-$BASE_DIR/cutter-gtk --color=auto \
+CUT_OUTPUT_MODULE_DIR=../../cutter/module/output/.libs $BASE_DIR/cutter-gtk --color=auto \
     -s $BASE_DIR "$@" $BASE_DIR
