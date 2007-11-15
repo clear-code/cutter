db2a181071dde1f0bbc5ade3931b7fc7cf72f431
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2007-11-15 05:25:29 +0000

    2007-11-15  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* sample/cairo/*: Added sample test for cairo.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@628 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ sample/cairo/run-test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export BASE_DIR="`dirname $0`"
+
+if test x"$NO_MAKE" != x"yes"; then
+    make -C $BASE_DIR/../../ > /dev/null || exit 1
+fi
+
+export CUT_OUTPUT_MODULE_DIR=$BASE_DIR/../../cutter/module/output/.libs
+$BASE_DIR/../../cutter/cutter \
+    --color=auto --multi-thread -s $BASE_DIR "$@" $BASE_DIR
