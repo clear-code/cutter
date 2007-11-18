f71fa3352f048180d5636628fd63c39557e8968f
Kouhei Sutou <kou@cozmixng.org>
2007-11-18 11:59:15 +0000

    * **/run-test.sh: output -> runner.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@700 10a583ed-1e3d-0410-b285-c80daaaae891

--- sample/glib/run-test.sh
+++ sample/glib/run-test.sh
@@ -6,6 +6,6 @@ if test x"$NO_MAKE" != x"yes"; then
     make -C $BASE_DIR/../../ > /dev/null || exit 1
 fi
 
-export CUT_OUTPUT_MODULE_DIR=$BASE_DIR/../../cutter/module/output/.libs
+export CUT_RUNNER_MODULE_DIR=$BASE_DIR/../../cutter/module/runner/.libs
 $BASE_DIR/../../cutter/cutter \
     --color=auto --multi-thread -s $BASE_DIR "$@" $BASE_DIR
--- test/run-test.sh
+++ test/run-test.sh
@@ -7,5 +7,5 @@ if test x"$NO_MAKE" != x"yes"; then
 fi
 
 export CUT_RUNNER_MODULE_DIR=$BASE_DIR/../cutter/module/runner/.libs
-$BASE_DIR/../cutter/cutter --color=auto \
+$BASE_DIR/../cutter/cutter --color=auto --multi-thread \
     -s $BASE_DIR "$@" $BASE_DIR
