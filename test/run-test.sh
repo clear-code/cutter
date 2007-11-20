f095969085560f34526b8895d4508aa334f8ecdc
Kouhei Sutou <kou@cozmixng.org>
2007-11-20 02:45:36 +0000

    * configure.ac: exported cutter program path for internal test. * test/run-test.s, test/Makefile.am: used CUTTER.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@730 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/run-test.sh
+++ test/run-test.sh
@@ -6,6 +6,9 @@ if test x"$NO_MAKE" != x"yes"; then
     make -C $BASE_DIR/../ > /dev/null || exit 1
 fi
 
+if test -z "$CUTTER"; then
+    CUTTER="$BASE_DIR/`make -s -C $BASE_DIR echo-cutter`"
+fi
+
 export CUT_RUNNER_MODULE_DIR=$BASE_DIR/../cutter/module/runner/.libs
-$BASE_DIR/../cutter/cutter --color=auto --multi-thread \
-    -s $BASE_DIR "$@" $BASE_DIR
+$CUTTER --color=auto --multi-thread -s $BASE_DIR "$@" $BASE_DIR
