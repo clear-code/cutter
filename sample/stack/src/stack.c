20aefb9bb79840fbc47dbc8fd1a63f3c9a9b44bf
Kouhei Sutou <kou@cozmixng.org>
2007-11-20 06:30:52 +0000

    * updated tutorial.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@743 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ sample/stack/src/stack.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "stack.h"
+
+#define TRUE 1
+#define FALSE 0
+
+Stack *
+stack_new (void)
+{
+    return NULL;
+}
+
+int
+stack_is_empty (Stack *stack)
+{
+    return TRUE;
+}
