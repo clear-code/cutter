a6652f4ad8f712b6653ca2c4765d004f2b4761df
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-02-26 05:18:01 +0000

    update.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1103 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/report_test_dir/dummy-test-functions.c
+++ test/report_test_dir/dummy-test-functions.c
@@ -1,37 +1,10 @@
 #include <cutter.h>
 
-/* BUG entries */
-const char *bug_get_bug_id (void);
-
-/* metadata */
-const CutTestMetadata *meta_metadata (void);
-
 /* tests */
-void test_get_bug_id (void);
-void test_metadata (void);
-
-const char *bug_get_bug_id (void) { return "1234567890"; }
-void
-test_get_bug_id (void)
-{
-}
-
-
-const CutTestMetadata *
-meta_metadata (void)
-{
-    static const CutTestMetadata metadata[] = {
-        {"bug", "1234"},
-        {"priority", "5678"},
-        {"bug", "9"},
-        {NULL, NULL}
-    };
-
-    return metadata;
-}
+void test_success (void);
 
 void
-test_metadata (void)
+test_success (void)
 {
 }
 
