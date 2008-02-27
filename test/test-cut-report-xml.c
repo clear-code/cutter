c51a17729e3070f474690e2875440bdb409a19e9
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-02-27 22:49:35 +0000

    * test/test-cut-report-xml.c: Set independent test context.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1122 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test-cut-report-xml.c
+++ test/test-cut-report-xml.c
@@ -2,34 +2,34 @@
 #include "cut-runner.h"
 #include "cut-report.h"
 
-void test_report_success (void);
-#if 0
+/* void test_report_success (void); */
 void test_report_failure (void);
-#endif
 
 static CutRunner *runner;
 static CutReport *report;
 static CutTest *test_object;
 static CutTestCase *test_case;
+static CutTestContext *test_context;
 
+#if 0
 static void
 dummy_success_test (void)
 {
 }
+#endif
 
-#if 0
 static void
 dummy_failure_test (void)
 {
     cut_fail("This test should fail");
 }
-#endif
 
 void
 setup (void)
 {
     const gchar *test_names[] = {"/.*/", NULL};
     test_object = NULL;
+    test_context = NULL;
     cut_report_load(NULL);
     runner = cut_runner_new();
     cut_runner_set_target_test_names(runner, test_names);
@@ -45,24 +45,45 @@ teardown (void)
 {
     if (test_object)
         g_object_unref(test_object);
+    if (test_context)
+        g_object_unref(test_context);
     g_object_unref(test_case);
     g_object_unref(report);
     g_object_unref(runner);
     cut_report_unload();
 }
 
+#if 0
 static void
 cb_success_signal (CutTest *test, CutTestResult *result, gpointer data)
 {
     g_object_set(G_OBJECT(result), "elapsed", 0.0001, NULL);
 }
+#endif
 
-static gboolean
-run_the_test (void)
+static void
+cb_failure_signal (CutTest *test, CutTestContext *context, CutTestResult *result, gpointer data)
 {
-    return cut_test_case_run(test_case, runner);
+    g_object_set(G_OBJECT(result), "elapsed", 0.0001, NULL);
 }
 
+static gboolean
+run_the_test (CutTest *test)
+{
+    gboolean success;
+    CutTestContext *original_test_context;
+
+    test_context = cut_test_context_new(NULL, test_case, test);
+    original_test_context = get_current_test_context();
+    set_current_test_context(test_context);
+    success = cut_test_run(test, test_context, runner);
+    set_current_test_context(original_test_context);
+
+    g_object_unref(test_context);
+    return success;
+}
+
+#if 0
 void
 test_report_success (void)
 {
@@ -80,21 +101,21 @@ test_report_success (void)
     test_object = cut_test_new("dummy-success-test", NULL, dummy_success_test);
     g_signal_connect_after(test_object, "success", G_CALLBACK(cb_success_signal), NULL);
     cut_test_case_add_test(test_case, test_object);
-    cut_assert(run_the_test());
+    cut_assert(run_the_test(test_object));
     g_signal_handlers_disconnect_by_func(test_object,
                                          G_CALLBACK(cb_success_signal),
                                          NULL);
 
     cut_assert_equal_string_with_free(expected, cut_report_get_success_results(report));
 }
+#endif
 
-#if 0
 void
 test_report_failure (void)
 {
     gchar expected[] = "  <test_log>\n"
                        "    <test_case>\n"
-                       "      <name>dummy_report_test</name>\n"
+                       "      <name>dummy test case</name>\n"
                        "    </test_case>\n"
                        "    <name>dummy-failure-test</name>\n"
                        "    <result>\n"
@@ -103,15 +124,17 @@ test_report_failure (void)
                        "      <elapsed>0.0001</elapsed>\n"
                        "    </result>\n"
                        "  </test_log>\n";
-    CutTest *test;
 
-    test = cut_test_new("dummy-failure-test", NULL, dummy_failure_test);
-    cut_test_case_add_test(test_case, test);
-    cut_assert(run_the_test());
+    test_object = cut_test_new("dummy-failure-test", NULL, dummy_failure_test);
+    g_signal_connect_after(test_object, "failure", G_CALLBACK(cb_failure_signal), NULL);
+    cut_test_case_add_test(test_case, test_object);
+    cut_assert(!run_the_test(test_object));
+    g_signal_handlers_disconnect_by_func(test_object,
+                                         G_CALLBACK(cb_failure_signal),
+                                         NULL);
 
     cut_assert_equal_string_with_free(expected, cut_report_get_failure_results(report));
 }
-#endif
 
 /*
 vi:ts=4:nowrap:ai:expandtab:sw=4
