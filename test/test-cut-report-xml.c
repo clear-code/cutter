0faa437ee84a19d5399533d12c4a7d28dc02c726
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-02-28 22:17:42 +0000

    2008-02-29  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test-cut-report-xml.c: Adapt to new format.
    
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1145 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test-cut-report-xml.c
+++ test/test-cut-report-xml.c
@@ -93,17 +93,17 @@ run_the_test (CutTest *test)
 void
 test_report_success (void)
 {
-    gchar expected[] = "  <test_log>\n"
+    gchar expected[] = "  <result>\n"
                        "    <test_case>\n"
                        "      <name>dummy test case</name>\n"
                        "    </test_case>\n"
-                       "    <name>dummy-success-test</name>\n"
-                       "    <result>\n"
-                       "      <status>success</status>\n"
-                       "      <detail></detail>\n"
-                       "      <elapsed>0.0001</elapsed>\n"
-                       "    </result>\n"
-                       "  </test_log>\n";
+                       "    <test>\n"
+                       "      <name>dummy-success-test</name>\n"
+                       "    </test>\n"
+                       "    <status>success</status>\n"
+                       "    <detail></detail>\n"
+                       "    <elapsed>0.0001</elapsed>\n"
+                       "  </result>\n";
     test_object = cut_test_new("dummy-success-test", NULL, dummy_success_test);
     g_signal_connect_after(test_object, "success", G_CALLBACK(cb_success_signal), NULL);
     cut_test_case_add_test(test_case, test_object);
@@ -118,17 +118,17 @@ test_report_success (void)
 void
 test_report_failure (void)
 {
-    gchar expected[] = "  <test_log>\n"
+    gchar expected[] = "  <result>\n"
                        "    <test_case>\n"
                        "      <name>dummy test case</name>\n"
                        "    </test_case>\n"
-                       "    <name>dummy-failure-test</name>\n"
-                       "    <result>\n"
-                       "      <status>failure</status>\n"
-                       "      <detail></detail>\n"
-                       "      <elapsed>0.0001</elapsed>\n"
-                       "    </result>\n"
-                       "  </test_log>\n";
+                       "    <test>\n"
+                       "      <name>dummy-failure-test</name>\n"
+                       "    </test>\n"
+                       "    <status>failure</status>\n"
+                       "    <detail></detail>\n"
+                       "    <elapsed>0.0001</elapsed>\n"
+                       "  </result>\n";
 
     test_object = cut_test_new("dummy-failure-test", NULL, dummy_failure_test);
     g_signal_connect_after(test_object, "failure", G_CALLBACK(cb_failure_signal), NULL);
