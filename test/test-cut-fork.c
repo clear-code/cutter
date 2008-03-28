f435573a0892fa194e90618009c4951b5163a1da
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-03-28 00:49:34 +0000

    2008-03-28  Hiroyuki Ikezoe  <poincare@ikezoe.net>
    	* test/test-cut-fork.c: Added sample test.
    
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1356 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/test-cut-fork.c
@@ -0,0 +1,46 @@
+#include "cutter.h"
+
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void test_fork (void);
+
+void
+setup (void)
+{
+}
+
+void
+teardown (void)
+{
+}
+
+void
+test_fork (void)
+{
+    pid_t pid;
+    int status;
+
+    pid = fork();
+
+    if (pid == 0) {
+        cut_notify("Walk in child process");
+        cut_fail("An error was occured");
+        _exit(0);
+    } else if (pid > 0) {
+        int wr;
+        cut_notify("Go on parent process");
+        do
+            wr = waitpid(pid, &status, 0);
+        while (wr < 0 && errno == EINTR);
+        cut_notify("End of child process");
+    } else {
+        cut_fail("Failed to create a child process");
+    }
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
