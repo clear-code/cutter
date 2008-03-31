bf5aa718d9d8ad95ad5e0e900283712c396cb2a1
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-03-31 02:01:28 +0000

    Use get_stdout_message().
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1365 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test-cut-fork.c
+++ test/test-cut-fork.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <glib.h>
 
 void test_fork (void);
 
@@ -23,11 +24,11 @@ test_fork (void)
     int pid;
     int status;
 
-    pid = cut_fork(10);
+    pid = cut_fork(0);
 
     if (pid == 0) {
-        cut_notify("Walk in child process");
-        cut_fail("An error was occured");
+        g_print("Walk in child process");
+        g_printerr("An error was occured");
         _exit(0);
     } else if (pid > 0) {
         int wr;
@@ -35,6 +36,8 @@ test_fork (void)
         do
             wr = waitpid(pid, &status, 0);
         while (wr < 0 && errno == EINTR);
+        cut_assert_equal_string("Walk in child proccess", cut_fork_get_stdout_message(pid));
+        cut_assert_equal_string("An error was occured", cut_fork_get_stderr_message(pid));
         cut_notify("End of child process");
     } else {
         cut_fail("Failed to create a child process");
