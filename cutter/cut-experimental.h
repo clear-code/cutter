5a95c4de61584bcae57afa8d509183e0f7d44838
Kouhei Sutou <kou@cozmixng.org>
2008-05-07 04:50:01 +0000

    * cutter/cut-experimental.h: add document.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1662 10a583ed-1e3d-0410-b285-c80daaaae891

--- cutter/cut-experimental.h
+++ cutter/cut-experimental.h
@@ -25,6 +25,14 @@ extern "C" {
 #endif
 
 /**
+ * SECTION: cut-experimental
+ * @title: Experimental
+ * @short_description: Experimental API.
+ *
+ * This API may be changed in the future.
+ */
+
+/**
  * cut_user_data:
  *
  * This is useful only if you make a custom test runner like
@@ -42,18 +50,116 @@ typedef struct _CutTestAttribute {
     const char *value;
 } CutTestAttribute;
 
+/**
+ * cut_fork:
+ *
+ * Makes child process.
+ *
+ * e.g.:
+ * |[
+ * int pid;
+ * pid = cut_fork();
+ * cut_assert_errno();
+ *
+ * if (pid == 0) {
+ *   do_something_for_child_process();
+ *   _exit(EXIT_SUCCESS);
+ * }
+ *
+ * do_something_for_parent_process();
+ * ]|
+ *
+ * Returns: pid.
+ *
+ * Since: 0.8
+ */
 #define cut_fork()                                              \
     cut_test_context_trap_fork(get_current_test_context(),      \
                                __PRETTY_FUNCTION__,             \
                                __FILE__,                        \
                                __LINE__)
 
+/**
+ * cut_wait_process:
+ * @pid: Process ID to wait.
+ * @uesc_timeout: Timeout in usec.
+ *
+ * Waits a process specified by @pid.
+ *
+ * e.g.:
+ * |[
+ * int pid;
+ * pid = cut_fork();
+ * cut_assert_errno();
+ *
+ * if (pid == 0) {
+ *   do_something_for_child_process();
+ *   _exit(EXIT_SUCCESS);
+ * }
+ *
+ * do_something_for_parent_process();
+ * cut_assert_equal_int(EXIT_SUCCESS, cut_wait_process(pid, 100));
+ * ]|
+ *
+ * Since: 0.8
+ */
 #define cut_wait_process(pid, usec_timeout)                     \
     cut_test_context_wait_process(get_current_test_context(),   \
                                   pid, usec_timeout)
 
+/**
+ * cut_fork_get_stdout_message:
+ * @pid: Process ID to be read from stdout.
+ *
+ * Reads a message from a process specified by @pid.
+ *
+ * e.g.:
+ * |[
+ * int pid;
+ * pid = cut_fork();
+ * cut_assert_errno();
+ *
+ * if (pid == 0) {
+ *   g_print("I'm a child.");
+ *   _exit(EXIT_SUCCESS);
+ * }
+ *
+ * cut_assert_equal_string("I'm a child.", cut_fork_get_stdout_message(pid));
+ * cut_assert_equal_int(EXIT_SUCCESS, cut_wait_process(pid, 100));
+ * ]|
+ *
+ * Returns: a message read from stdout of a process specified by @pid.
+ *
+ * Since: 0.8
+ */
 #define cut_fork_get_stdout_message(pid)                            \
     cut_test_context_get_forked_stdout_message(get_current_test_context(), pid)
+
+/**
+ * cut_fork_get_stderr_message:
+ * @pid: Process ID to be read from stderr.
+ *
+ * Reads a message from a process specified by @pid.
+ *
+ * e.g.:
+ * |[
+ * int pid;
+ * pid = cut_fork();
+ * cut_assert_errno();
+ *
+ * if (pid == 0) {
+ *   g_print("I'm a child.");
+ *   _exit(EXIT_SUCCESS);
+ * }
+ *
+ * cut_assert_equal_string("I'm a child.", cut_fork_get_stderr_message(pid));
+ * cut_assert_equal_int(EXIT_SUCCESS, cut_wait_process(pid, 100));
+ * ]|
+ *
+ * Returns: a message read from stderr of a process specified by @pid.
+ *
+ * Since: 0.8
+ */
 #define cut_fork_get_stderr_message(pid)                            \
     cut_test_context_get_forked_stderr_message(get_current_test_context(), pid)
 
