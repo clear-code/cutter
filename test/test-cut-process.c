a777d8d3360fd847d50eae5dc78cdc8e1017d2e9
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-05-15 01:36:14 +0000

    added test-cut-process.c.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1727 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/test-cut-process.c
@@ -0,0 +1,37 @@
+#include "cutter.h"
+#include <cutter/cut-process.h>
+
+#include <unistd.h>
+
+void test_fork (void);
+
+static CutProcess *process;
+
+void
+setup (void)
+{
+    process = cut_process_new();
+}
+
+void
+teardown (void)
+{
+    g_object_unref(process);
+}
+
+void
+test_fork (void)
+{
+    int pid;
+    pid = cut_process_fork(process);
+
+    if (pid == 0) {
+        _exit(EXIT_SUCCESS);
+    }
+
+    cut_assert_equal_int (EXIT_SUCCESS, cut_process_wait(process, 0));
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
