9d1a169e8b404b92f8d612f06b1f6e8e919c1930
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-05-16 06:31:56 +0000

    added some signal tests. these are failed yet.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1771 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test-cut-pipeline.c
+++ test/test-cut-pipeline.c
@@ -6,6 +6,9 @@
 
 void test_exit_status_error (void);
 void test_exit_status_success (void);
+void test_failure_signal(void);
+void test_error_signal(void);
+void test_pending_signal(void);
 
 static CutRunContext *pipeline;
 static gboolean received_complete_signal = FALSE;
@@ -77,6 +80,54 @@ test_exit_status_success (void)
                                          G_CALLBACK(cb_complete_run_signal),
                                          &success);
 }
+
+#define DEFINE_ERROR_SIGNAL_TEST(signal_name)           \
+static void                                             \
+cb_##signal_name##_signal (CutRunContext *run_context,  \
+                   CutTest *test,                       \
+                   CutTestContext *test_context,        \
+                   CutTestResult *result,               \
+                   gpointer data)                       \
+{                                                       \
+    gint *n_signal;                                     \
+                                                        \
+    n_signal = (gint*)data;                             \
+    n_signal++;                                         \
+}                                                       \
+                                                        \
+void                                                    \
+test_##signal_name##_signal (void)                      \
+{                                                       \
+    gboolean success = FALSE;                           \
+    gint n_signal = 0;                                  \
+                                                        \
+    pipeline = cut_pipeline_new("./pipeline_test_dir/error_test/"); \
+    cut_assert(pipeline);                               \
+                                                        \
+    g_signal_connect(pipeline, "complete-run",          \
+                     G_CALLBACK(cb_complete_run_signal), &success); \
+    g_signal_connect(pipeline, #signal_name"-test",     \
+                     G_CALLBACK(cb_##signal_name##_signal), &n_signal); \
+                                                        \
+    cut_runner_run_async(CUT_RUNNER(pipeline));         \
+    while (!received_complete_signal) {                 \
+        g_main_context_iteration(NULL, FALSE);          \
+    }                                                   \
+                                                        \
+    cut_assert_false(success);                          \
+    cut_assert_equal_int(1, n_signal);                  \
+                                                        \
+    g_signal_handlers_disconnect_by_func(pipeline,      \
+                                         G_CALLBACK(cb_complete_run_signal),    \
+                                         &success);     \
+    g_signal_handlers_disconnect_by_func(pipeline,      \
+                                         G_CALLBACK(cb_##signal_name##_signal), \
+                                         &n_signal);    \
+}
+
+DEFINE_ERROR_SIGNAL_TEST(failure)
+DEFINE_ERROR_SIGNAL_TEST(error)
+DEFINE_ERROR_SIGNAL_TEST(pending)
 
 /*
 vi:ts=4:nowrap:ai:expandtab:sw=4
