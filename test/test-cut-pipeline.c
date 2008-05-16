4f4f5bbc06142921f6e2f6cb58b1714a7ff22c99
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-05-16 04:36:18 +0000

    added error and success test. succes test is failed yet.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1758 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/test-cut-pipeline.c
+++ test/test-cut-pipeline.c
@@ -4,7 +4,8 @@
 
 #include <unistd.h>
 
-void test_fork (void);
+void test_error (void);
+void test_success (void);
 
 static CutRunContext *pipeline;
 static gboolean received_complete_signal = FALSE;
@@ -24,28 +25,57 @@ teardown (void)
 }
 
 static void
-cb_complete_run_signal (CutPipeline *pipeline, gboolean success)
+cb_complete_run_signal (CutPipeline *pipeline, gboolean success, gpointer user_data)
 {
+    gboolean *is_success;
     received_complete_signal = TRUE;
+
+    is_success = (gboolean*)user_data;
+    *is_success = success;
 }
 
 void
-test_fork (void)
+test_error (void)
 {
-    pipeline = cut_pipeline_new("./pipeline_test_dir");
+    gboolean success = TRUE;
+
+    pipeline = cut_pipeline_new("./pipeline_test_dir/error_test/");
     cut_assert(pipeline);
 
     g_signal_connect(pipeline, "complete-run",
-                     G_CALLBACK(cb_complete_run_signal), NULL);
+                     G_CALLBACK(cb_complete_run_signal), &success);
 
     cut_runner_run_async(CUT_RUNNER(pipeline));
     while (!received_complete_signal) {
         g_main_context_iteration(NULL, FALSE);
     }
 
+    cut_assert(!success);
     g_signal_handlers_disconnect_by_func(pipeline,
                                          G_CALLBACK(cb_complete_run_signal),
-                                         NULL);
+                                         &success);
+}
+
+void
+test_success (void)
+{
+    gboolean success = FALSE;
+
+    pipeline = cut_pipeline_new("./pipeline_test_dir/success_test/");
+    cut_assert(pipeline);
+
+    g_signal_connect(pipeline, "complete-run",
+                     G_CALLBACK(cb_complete_run_signal), &success);
+
+    cut_runner_run_async(CUT_RUNNER(pipeline));
+    while (!received_complete_signal) {
+        g_main_context_iteration(NULL, FALSE);
+    }
+
+    cut_assert(success);
+    g_signal_handlers_disconnect_by_func(pipeline,
+                                         G_CALLBACK(cb_complete_run_signal),
+                                         &success);
 }
 
 /*
