d9d51bb8fe861e9e30736838c9aa821488e92210
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-05-20 01:33:26 +0000

    added CUT_STREAMER_XX environment values.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1883 10a583ed-1e3d-0410-b285-c80daaaae891

--- sample/cairo/run-test.sh
+++ sample/cairo/run-test.sh
@@ -10,6 +10,8 @@ export CUT_UI_MODULE_DIR=$BASE_DIR/../../cutter/module/ui/.libs
 export CUT_UI_FACTORY_MODULE_DIR=$BASE_DIR/../../cutter/module/ui/.libs
 export CUT_REPORT_MODULE_DIR=$BASE_DIR/../../cutter/module/report/.libs
 export CUT_REPORT_FACTORY_MODULE_DIR=$BASE_DIR/../../cutter/module/report/.libs
+export CUT_STREAMER_MODULE_DIR=$BASE_DIR/../../cutter/module/streamer/.libs
+export CUT_STREAMER_FACTORY_MODULE_DIR=$BASE_DIR/../../cutter/module/streamer/.libs
 
 $BASE_DIR/cutter-cairo \
     --color=auto --multi-thread -s $BASE_DIR "$@" $BASE_DIR
