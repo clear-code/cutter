9fc4118ca613d4f853b07fd2416d29aca0347511
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-05-27 06:20:08 +0000

    cleanup.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1970 10a583ed-1e3d-0410-b285-c80daaaae891

--- gst-plugins/run-test.sh
+++ gst-plugins/run-test.sh
@@ -6,7 +6,6 @@ if test x"$NO_MAKE" != x"yes"; then
     make -C $BASE_DIR/../ > /dev/null || exit 1
 fi
 
-export CUTTER
 export CUT_UI_MODULE_DIR=$BASE_DIR/../module/ui/.libs
 export CUT_UI_FACTORY_MODULE_DIR=$BASE_DIR/../module/ui/.libs
 export CUT_REPORT_MODULE_DIR=$BASE_DIR/../module/report/.libs
