01b14970dbb59bf693486962e636fa7c826a463d
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2008-05-27 03:30:31 +0000

    added test script.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/trunk@1960 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ gst-plugins/run-test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+export BASE_DIR="`dirname $0`"
+
+if test x"$NO_MAKE" != x"yes"; then
+    make -C $BASE_DIR/../ > /dev/null || exit 1
+fi
+
+export CUTTER
+export CUT_UI_MODULE_DIR=$BASE_DIR/../module/ui/.libs
+export CUT_UI_FACTORY_MODULE_DIR=$BASE_DIR/../module/ui/.libs
+export CUT_REPORT_MODULE_DIR=$BASE_DIR/../module/report/.libs
+export CUT_REPORT_FACTORY_MODULE_DIR=$BASE_DIR/../module/report/.libs
+export CUT_STREAMER_MODULE_DIR=$BASE_DIR/../module/streamer/.libs
+export CUT_STREAMER_FACTORY_MODULE_DIR=$BASE_DIR/../module/streamer/.libs
+
+export GST_PLUGIN_PATH=.libs
+gst-launch-0.10 cutter-test-runner test-directory=. ! cutter-test-viewer
+
