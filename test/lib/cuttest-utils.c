c1c5b4a08565dd99ca50c5c1ea1b0959bae40c1a
Kouhei Sutou <kou@cozmixng.org>
2008-07-12 09:40:35 +0000

    * test/lib/cuttest-utils.[ch] (cuttest_result_summary_list_new)   (cuttest_result_summary_list_new_from_run_context): add.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2137 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/lib/cuttest-utils.c
+++ test/lib/cuttest-utils.c
@@ -19,3 +19,46 @@ cuttest_add_test (CutTestCase *test_case, const gchar *test_name,
     cut_test_case_add_test(test_case, test);
     g_object_unref(test);
 }
+
+GList *
+cuttest_result_summary_list_new (guint n_tests,
+                                 guint n_assertions,
+                                 guint n_successes,
+                                 guint n_failures,
+                                 guint n_errors,
+                                 guint n_pendings,
+                                 guint n_notifications,
+                                 guint n_omissions)
+{
+    GList *list = NULL;
+
+#define APPEND(uint_value)                                      \
+    list = g_list_append(list, GUINT_TO_POINTER(uint_value));   \
+
+    APPEND(n_tests);
+    APPEND(n_assertions);
+    APPEND(n_failures);
+    APPEND(n_errors);
+    APPEND(n_pendings);
+    APPEND(n_notifications);
+    APPEND(n_omissions);
+
+#undef APPEND
+
+    return list;
+}
+
+GList *
+cuttest_result_summary_list_new_from_run_context (CutRunContext *run_context)
+{
+    return cuttest_result_summary_list_new(
+        cut_run_context_get_n_tests(run_context),
+        cut_run_context_get_n_assertions(run_context),
+        0,
+        cut_run_context_get_n_failures(run_context),
+        cut_run_context_get_n_errors(run_context),
+        cut_run_context_get_n_pendings(run_context),
+        cut_run_context_get_n_notifications(run_context),
+        cut_run_context_get_n_omissions(run_context));
+}
+
--- test/lib/cuttest-utils.h
+++ test/lib/cuttest-utils.h
@@ -3,10 +3,22 @@
 
 #include <glib.h>
 #include <cutter/cut-test-case.h>
+#include <cutter/cut-run-context.h>
 
 const gchar *cuttest_get_base_dir (void);
 void         cuttest_add_test     (CutTestCase *test_case,
                                    const gchar *test_name,
                                    CutTestFunction test_function);
+GList       *cuttest_result_summary_list_new
+                                  (guint n_tests,
+                                   guint n_assertions,
+                                   guint n_successes,
+                                   guint n_failures,
+                                   guint n_errors,
+                                   guint n_pendings,
+                                   guint n_notifications,
+                                   guint n_omissions);
+GList       *cuttest_result_summary_list_new_from_run_context
+                                  (CutRunContext *run_context);
 
 #endif
