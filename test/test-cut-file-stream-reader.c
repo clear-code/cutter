71ced7e5822f89a9aed20baa6bee23c6bf3d0829
Kouhei Sutou <kou@cozmixng.org>
2008-08-26 01:30:11 +0000

    * test/test-cut-file-stream-reader.c, test/fixtures/file-stream-reader/, test/fixtures/Makefile.am: add tests for file stream reader.
    * test/Makefile.am: add test_cut_file_stream_reader.so.
    
    * configure.ac: add test/fixtures/file-stream-reader/Makefile.
    
    
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2333 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/test-cut-file-stream-reader.c
@@ -0,0 +1,58 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#include <gcutter.h>
+#include <cutter/cut-file-stream-reader.h>
+
+#include "lib/cuttest-assertions.h"
+
+void test_start_all_success (void);
+void test_start_error_test (void);
+
+static CutRunContext *reader;
+static gchar *path;
+
+void
+setup (void)
+{
+    reader = NULL;
+    path = NULL;
+
+    cut_set_fixture_data_dir(cuttest_get_base_dir(),
+                             "fixtures",
+                             "file-stream-reader",
+                             NULL);
+}
+
+void
+teardown (void)
+{
+    if (reader)
+        g_object_unref(reader);
+
+    if (path)
+        g_free(path);
+}
+
+void
+test_start_all_success (void)
+{
+    path = cut_build_fixture_data_path("all-success.xml", NULL);
+    reader = cut_file_stream_reader_new(path);
+
+    cut_assert_true(cut_run_context_start(reader));
+    cut_assert_test_result_summary(reader, 19, 49, 19, 0, 0, 0, 0, 0);
+}
+
+void
+test_start_error_test (void)
+{
+    path = cut_build_fixture_data_path("error-test.xml", NULL);
+    reader = cut_file_stream_reader_new(path);
+
+    cut_assert_false(cut_run_context_start(reader));
+    cut_assert_test_result_summary(reader, 6, 16, 4, 1, 1, 0, 0, 0);
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
