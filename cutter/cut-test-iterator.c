dc506d79d5e2f37f0c19c978e0eb0457f1305e83
Kouhei Sutou <kou@cozmixng.org>
2008-10-04 12:44:55 +0000

    * cutter/cut-test-iterator.c: cleanup.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2440 10a583ed-1e3d-0410-b285-c80daaaae891

--- cutter/cut-test-iterator.c
+++ cutter/cut-test-iterator.c
@@ -363,6 +363,99 @@ cb_test_status (CutTest *test, CutTestContext *context, CutTestResult *result,
     *status = MAX(*status, cut_test_result_get_status(result));
 }
 
+static void
+run_iterated_tests (CutTest *test, CutTestContext *test_context,
+                    CutRunContext *run_context,
+                    CutTestResultStatus *status, gboolean *all_success)
+{
+    CutTestIteratorPrivate *priv;
+    CutTestIterator *test_iterator;
+    CutTestContainer *test_container;
+    GError *error = NULL;
+    GList *node, *iterated_tests = NULL, *filtered_tests = NULL;
+    const gchar **test_names;
+    guint n_tests;
+    GThreadPool *thread_pool = NULL;
+
+    thread_pool = g_thread_pool_new(run_test_without_thread,
+                                    all_success,
+                                    cut_run_context_get_max_threads(run_context),
+                                    FALSE,
+                                    &error);
+    if (error) {
+        gchar *inspected;
+
+        inspected = gcut_error_inspect(error);
+        g_warning("%s", inspected);
+        g_free(inspected);
+        g_error_free(error);
+        return;
+    }
+
+    priv = CUT_TEST_ITERATOR_GET_PRIVATE(test);
+    test_iterator = CUT_TEST_ITERATOR(test);
+    while (cut_test_context_have_data(test_context)) {
+        CutIteratedTest *iterated_test;
+        CutTestData *test_data;
+
+        test_data = cut_test_context_get_current_data(test_context);
+        iterated_test = cut_iterated_test_new(cut_test_get_name(test),
+                                              priv->iterated_test_function,
+                                              test_data);
+        cut_test_iterator_add_test(test_iterator, iterated_test);
+        g_object_unref(iterated_test);
+
+        iterated_tests = g_list_prepend(iterated_tests, iterated_test);
+
+        g_signal_connect(iterated_test, "success",
+                         G_CALLBACK(cb_test_status), status);
+        g_signal_connect(iterated_test, "failure",
+                         G_CALLBACK(cb_test_status), status);
+        g_signal_connect(iterated_test, "error",
+                         G_CALLBACK(cb_test_status), status);
+        g_signal_connect(iterated_test, "pending",
+                         G_CALLBACK(cb_test_status), status);
+        g_signal_connect(iterated_test, "notification",
+                         G_CALLBACK(cb_test_status), status);
+        g_signal_connect(iterated_test, "omission",
+                         G_CALLBACK(cb_test_status), status);
+
+        cut_test_context_shift_data(test_context);
+    }
+
+    test_container = CUT_TEST_CONTAINER(test);
+    test_names = cut_run_context_get_target_test_names(run_context);
+    filtered_tests = cut_test_container_filter_children(test_container,
+                                                        test_names);
+
+    cut_run_context_prepare_test_iterator(run_context, test_iterator);
+    n_tests = cut_test_container_get_n_tests(test_container, run_context);
+    g_signal_emit_by_name(test_iterator, "ready", n_tests);
+    g_signal_emit_by_name(test, "start");
+
+    for (node = filtered_tests; node; node = g_list_next(node)) {
+        CutIteratedTest *iterated_test = node->data;
+
+        run_test_with_thread_support(test_iterator, iterated_test,
+                                     test_context, run_context,
+                                     thread_pool, all_success);
+    }
+    g_list_free(filtered_tests);
+
+    if (thread_pool)
+        g_thread_pool_free(thread_pool, FALSE, TRUE);
+
+    for (node = iterated_tests; node; node = g_list_next(node)) {
+        CutIteratedTest *iterated_test = node->data;
+
+        g_signal_handlers_disconnect_by_func(iterated_test,
+                                             G_CALLBACK(cb_test_status),
+                                             status);
+        cut_iterated_test_clear_data(iterated_test);
+    }
+    g_list_free(iterated_tests);
+}
+
 static gboolean
 run (CutTest *test, CutTestContext *test_context, CutRunContext *run_context)
 {
@@ -370,13 +463,8 @@ run (CutTest *test, CutTestContext *test_context, CutRunContext *run_context)
     CutTestIteratorPrivate *priv;
     CutTestResult *result;
     CutTestCase *test_case;
-    GList *node, *tests = NULL, *filtered_tests = NULL;
-    GThreadPool *thread_pool = NULL;
-    GError *error = NULL;
     CutTestResultStatus status = CUT_TEST_RESULT_SUCCESS;
     gboolean all_success = TRUE;
-    guint n_tests;
-    const gchar **test_names;
     jmp_buf jump_buffer;
 
     test_iterator = CUT_TEST_ITERATOR(test);
@@ -398,81 +486,8 @@ run (CutTest *test, CutTestContext *test_context, CutRunContext *run_context)
         return FALSE;
     }
 
-    thread_pool = g_thread_pool_new(run_test_without_thread,
-                                    &all_success,
-                                    cut_run_context_get_max_threads(run_context),
-                                    FALSE,
-                                    &error);
-    if (error) {
-        gchar *inspected;
-
-        inspected = gcut_error_inspect(error);
-        g_warning("%s", inspected);
-        g_free(inspected);
-        g_error_free(error);
-    }
-
-    while (cut_test_context_have_data(test_context)) {
-        CutIteratedTest *test;
-        CutTestData *test_data;
-
-        test_data = cut_test_context_get_current_data(test_context);
-        test = cut_iterated_test_new(cut_test_get_name(CUT_TEST(test_iterator)),
-                                     priv->iterated_test_function,
-                                     test_data);
-        cut_test_iterator_add_test(test_iterator, test);
-        g_object_unref(test);
-
-        tests = g_list_prepend(tests, test);
-
-        g_signal_connect(test, "success", G_CALLBACK(cb_test_status),
-                         &status);
-        g_signal_connect(test, "failure", G_CALLBACK(cb_test_status),
-                         &status);
-        g_signal_connect(test, "error", G_CALLBACK(cb_test_status),
-                         &status);
-        g_signal_connect(test, "pending", G_CALLBACK(cb_test_status),
-                         &status);
-        g_signal_connect(test, "notification", G_CALLBACK(cb_test_status),
-                         &status);
-        g_signal_connect(test, "omission", G_CALLBACK(cb_test_status),
-                         &status);
-
-        cut_test_context_shift_data(test_context);
-    }
-
-    test_names = cut_run_context_get_target_test_names(run_context);
-    filtered_tests =
-        cut_test_container_filter_children(CUT_TEST_CONTAINER(test_iterator),
-                                           test_names);
-
-    cut_run_context_prepare_test_iterator(run_context, test_iterator);
-    n_tests = cut_test_container_get_n_tests(CUT_TEST_CONTAINER(test_iterator),
-                                             run_context);
-    g_signal_emit_by_name(test_iterator, "ready", n_tests);
-    g_signal_emit_by_name(CUT_TEST(test_iterator), "start");
-
-    for (node = filtered_tests; node; node = g_list_next(node)) {
-        CutIteratedTest *test = node->data;
-
-        run_test_with_thread_support(test_iterator, test,
-                                     test_context, run_context,
-                                     thread_pool, &all_success);
-    }
-    g_list_free(filtered_tests);
-
-    if (thread_pool)
-        g_thread_pool_free(thread_pool, FALSE, TRUE);
-
-    for (node = tests; node; node = g_list_next(node)) {
-        CutIteratedTest *test = node->data;
-
-        g_signal_handlers_disconnect_by_func(test,
-                                             G_CALLBACK(cb_test_status),
-                                             &status);
-        cut_iterated_test_clear_data(test);
-    }
-    g_list_free(tests);
+    run_iterated_tests(test, test_context, run_context,
+                       &status, &all_success);
 
     test_case = cut_test_context_get_test_case(test_context);
     result = cut_test_result_new(status,
