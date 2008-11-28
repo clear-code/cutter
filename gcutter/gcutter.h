44c4ec2f1e0e8e3d094c27ca532da90043d62970
Kouhei Sutou <kou@cozmixng.org>
2008-11-28 03:39:46 +0000

    * gcutter/gcutter.h (gcut_add_datum): add.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2614 10a583ed-1e3d-0410-b285-c80daaaae891

--- gcutter/gcutter.h
+++ gcutter/gcutter.h
@@ -24,6 +24,70 @@
 #include <gcutter/gcut-main.h>
 #include <gcutter/gcut-assertions.h>
 
+/**
+ * gcut_add_datum:
+ * @name: The name of the data.
+ * @first_field_name: The first field name.
+ * @...: FIXME: The type and value of the field. NULL
+ *       terminated.
+ *
+ * Adds a datum to use data driven test. FIXME: It's
+ * convenient rather than cut_add_data() because you doesn't
+ * need to define a new data structure for a test.
+ *
+ * e.g.:
+ * |[
+ * #include <gcutter.h>
+ *
+ * void data_translate (void);
+ * void test_translate (gconstpointer data);
+ *
+ * static const gchar*
+ * translate (gint input)
+ * {
+ *    switch(input) {
+ *    case 1:
+ *        return "first";
+ *    case 111:
+ *        return "a hundred eleven";
+ *    default:
+ *        return "unsupported";
+ *    }
+ * }
+ *
+ * void
+ * data_translate(void)
+ * {
+ *     gcut_add_datum("simple data",
+ *                    "/translated", G_TYPE_STRING, "first",
+ *                    "/input", G_TYPE_INT, 1,
+ *                    NULL);
+ *     gcut_add_datum("complex data",
+ *                    "/translated", G_TYPE_STRING, "a hundred eleven",
+ *                    "/input", G_TYPE_INT, 111,
+ *                    NULL);
+ * }
+ *
+ * void
+ * test_translate(gconstpointer *_data)
+ * {
+ *     const GCutData *data = _data;
+ *
+ *     cut_assert_equal_string(gcut_data_get_string(data, "/translated"),
+ *                             gcut_data_get_int(data, "/input"));
+ * }
+ * ]|
+ *
+ * Since: 1.0.6
+ */
+#define gcut_add_datum(label, first_field_name, ...)    \
+    cut_test_context_add_data(                          \
+        cut_get_current_test_context(),                 \
+        label,                                          \
+        gcut_data_new(first_field_name, __VA_ARGS__),   \
+        g_object_unref,                                 \
+        NULL)
+
 #endif /* __GCUTTER_H__ */
 
 /*
