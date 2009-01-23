12d41317cbd080dbf6853087dbb7e6899819a69b
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2009-01-23 00:08:07 +0000

    * test/gcutter/test-gcut-egg.c: Use HAVE_SYS_WAIT_H.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2682 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/gcutter/test-gcut-egg.c
+++ test/gcutter/test-gcut-egg.c
@@ -4,7 +4,9 @@
 #include <errno.h>
 #include <locale.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+#ifdef HAVE_SYS_WAIT_H
+#  include <sys/wait.h>
+#endif
 #include <signal.h>
 
 #include <gcutter.h>
