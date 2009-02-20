e1415324fd1df24c75fe5867dec737959c1d7d61
Kouhei Sutou <kou@cozmixng.org>
2009-02-20 14:15:57 +0000

    * gcutter/gcut-inspect.h: add document.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2713 10a583ed-1e3d-0410-b285-c80daaaae891

--- gcutter/gcut-inspect.h
+++ gcutter/gcut-inspect.h
@@ -26,38 +26,167 @@ G_BEGIN_DECLS
 
 /**
  * SECTION: gcut-inspect
- * @title: WRITE ME
- * @short_description: WRITE ME
+ * @title: Object inspection functions.
+ * @short_description: Functions to inspect a object for
+ * debugging.
  *
- * WRITE ME
- *
- * WRITE ME
+ * In test result, we need to know detail of inspected
+ * objects for debugging. Functions of this section help us
+ * to inspect interested objects.
  *
  * Since: 1.0.6
  */
 
 
+/**
+ * gcut_inspect_direct:
+ * @string: the output string.
+ * @data: the interested target.
+ * @user_data: the data passed by user. (ignored)
+ *
+ * Shows @data as unsigned integer.
+ *
+ * e.g.:
+ * |[
+ * gcut_inspect_direct(string, GUINT_TO_POINTER(100), NULL) -> "100"
+ * ]|
+ *
+ * Since: 1.0.6
+ */
 void        gcut_inspect_direct       (GString       *string,
                                        gconstpointer  data,
                                        gpointer       user_data);
+
+/**
+ * gcut_inspect_int:
+ * @string: the output string.
+ * @data: the interested target.
+ * @user_data: the data passed by user. (ignored)
+ *
+ * Shows @data as integer.
+ *
+ * e.g.:
+ * |[
+ * gcut_inspect_nt(string, GINT_TO_POINTER(100), NULL) -> "100"
+ * ]|
+ *
+ * Since: 1.0.6
+ */
 void        gcut_inspect_int          (GString       *string,
                                        gconstpointer  data,
                                        gpointer       user_data);
+
+/**
+ * gcut_inspect_uint:
+ * @string: the output string.
+ * @data: the interested target.
+ * @user_data: the data passed by user. (ignored)
+ *
+ * Shows @data as unsigned integer.
+ *
+ * e.g.:
+ * |[
+ * gcut_inspect_uint(string, GUINT_TO_POINTER(100), NULL) -> "100"
+ * ]|
+ *
+ * Since: 1.0.6
+ */
 void        gcut_inspect_uint         (GString       *string,
                                        gconstpointer  data,
                                        gpointer       user_data);
+
+/**
+ * gcut_inspect_string:
+ * @string: the output string.
+ * @data: the interested target.
+ * @user_data: the data passed by user. (ignored)
+ *
+ * Shows @data as string. It also accepts %NULL.
+ *
+ * e.g.:
+ * |[
+ * gcut_inspect_string(string, "string", NULL) -> "\"string\""
+ * ]|
+ *
+ * Since: 1.0.6
+ */
 void        gcut_inspect_string       (GString       *string,
                                        gconstpointer  data,
                                        gpointer       user_data);
+
+/**
+ * gcut_inspect_type:
+ * @string: the output string.
+ * @data: the interested target.
+ * @user_data: the data passed by user. (ignored)
+ *
+ * Shows @data as %GType.
+ *
+ * e.g.:
+ * |[
+ * gcut_inspect_type(string, GTK_TYPE_WINDOW, NULL) -> "<Gtk::Window>" FIXME: confirm
+ * ]|
+ *
+ * Since: 1.0.6
+ */
 void        gcut_inspect_type         (GString       *string,
                                        gconstpointer  data,
                                        gpointer       user_data);
+
+/**
+ * gcut_inspect_flags:
+ * @string: the output string.
+ * @data: the interested target.
+ * @user_data: the data passed by user. (ignored)
+ *
+ * Shows @data as %GFlags.
+ *
+ * e.g.:
+ * |[
+ * FIXME
+ * ]|
+ *
+ * Since: 1.0.6
+ */
 void        gcut_inspect_flags        (GString       *string,
                                        gconstpointer  data,
                                        gpointer       user_data);
+
+/**
+ * gcut_inspect_enum:
+ * @string: the output string.
+ * @data: the interested target.
+ * @user_data: the data passed by user. (ignored)
+ *
+ * Shows @data as %GEnum.
+ *
+ * e.g.:
+ * |[
+ * FIXME
+ * ]|
+ *
+ * Since: 1.0.6
+ */
 void        gcut_inspect_enum         (GString       *string,
                                        gconstpointer  data,
                                        gpointer       user_data);
+
+/**
+ * gcut_inspect_pointer
+ * @string: the output string.
+ * @data: the interested target.
+ * @user_data: the data passed by user. (ignored)
+ *
+ * Shows @data as a pointer.
+ *
+ * e.g.:
+ * |[
+ * memory = malloc(1);
+ * gcut_inspect_pointer(string, memory, NULL) -> "#<0xXXXXXXX>"
+ * ]|
+ *
+ * Since: 1.0.6
+ */
 void        gcut_inspect_pointer      (GString       *string,
                                        gconstpointer  data,
                                        gpointer       user_data);
