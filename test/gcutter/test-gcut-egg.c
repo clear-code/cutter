0410ccf0b6b496d237343ac4e08d6d02c2a80300
Kouhei Sutou <kou@cozmixng.org>
2009-02-27 15:17:16 +0000

    * test/gcutter/test-gcut-egg.c: g_pid -> pid.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2727 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/gcutter/test-gcut-egg.c
+++ test/gcutter/test-gcut-egg.c
@@ -137,7 +137,7 @@ test_hatch (void)
     egg = gcut_egg_new("echo", "XXX", NULL);
     setup_egg(egg);
 
-    gcut_assert_equal_g_pid(0, gcut_egg_get_pid(egg));
+    gcut_assert_equal_pid(0, gcut_egg_get_pid(egg));
     gcut_egg_hatch(egg, &error);
     gcut_assert_error(error);
     cut_assert_operator_int(0, !=, gcut_egg_get_pid(egg));
