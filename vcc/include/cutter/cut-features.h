658b0135e782abf2056913ce10f434784163e423
Kouhei Sutou <kou@cozmixng.org>
2009-03-24 08:22:04 +0000

    * add a solution file for Visual C++ 2008 Express Edition. (sure, not work yet)
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2800 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ vcc/include/cutter/cut-features.h
@@ -0,0 +1,12 @@
+/* cut-features.h
+ *
+ * This is a generated file.  Please modify "configure.ac"
+ */
+
+#ifndef __CUT_FEATURES_H__
+#define __CUT_FEATURES_H__
+
+#define CUT_SUPPORT_GLIB 1
+#define CUT_SUPPORT_GDK_PIXBUF 1
+
+#endif
---
+++ vcc/include/cutter/cut-version.h
@@ -0,0 +1,87 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2008-2009  Kouhei Sutou <kou@cozmixng.org>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __CUT_VERSION_H__
+#define __CUT_VERSION_H__
+
+/**
+ * SECTION: cut-version
+ * @title: Version Information
+ * @short_description: Variables and macros to check the Cutter version
+ *
+ * Cutter provides version information, primarily useful in
+ * configure checks for builds that have a configure
+ * script. Applications will not typically use the features
+ * described here.
+ */
+
+/**
+ * CUTTER_VERSION_MAJOR:
+ *
+ * The major version number of the Cutter library.
+ */
+#define CUTTER_VERSION_MAJOR @CUTTER_VERSION_MAJOR@
+
+/**
+ * CUTTER_VERSION_MINOR:
+ *
+ * The minor version number of the Cutter library.
+ */
+#define CUTTER_VERSION_MINOR @CUTTER_VERSION_MINOR@
+
+/**
+ * CUTTER_VERSION_MICRO:
+ *
+ * The micro version number of the Cutter library.
+ */
+#define CUTTER_VERSION_MICRO @CUTTER_VERSION_MICRO@
+
+/**
+ * CUTTER_VERSION_STRING:
+ *
+ * The version number string of the Cutter library with
+ * "#{MAJOR}.#{MINOR}.#{MICRO}" format.
+ */
+#define CUTTER_VERSION_STRING "@CUTTER_VERSION@"
+
+/**
+ * CUTTER_CHECK_VERSION:
+ * @major: the major version number.
+ * @minor: the minor version number.
+ * @micro: the micro version number.
+ *
+ * Checks the version of the Cutter library. Returns true if
+ * the version of the Cutter header files is the same as or
+ * newer than the passed-in version.
+ *
+ * Since: 1.0.7
+ */
+#define CUTTER_CHECK_VERSION(major, minor, micro)       \
+    (CUTTER_VERSION_MAJOR > (major) ||                  \
+     (CUTTER_VERSION_MAJOR == (major) &&                \
+      CUTTER_VERSION_MINOR > (minor)) ||                \
+     (CUTTER_VERSION_MAJOR == (major) &&                \
+      CUTTER_VERSION_MINOR == (minor) &&                \
+      CUTTER_VERSION_MICRO >= (micro)))
+
+#endif /* __CUT_VERSION_H__ */
+
+/*
+vi:nowrap:ai:expandtab:sw=4
+*/
