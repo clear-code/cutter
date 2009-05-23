8b314a8b3fbdfcc6e793ea6c1fee39fe2ab9b0ba
Kouhei Sutou <kou@cozmixng.org>
2009-05-23 14:50:15 +0000

    * misc/generate-def.rb: collect missing function names.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2966 10a583ed-1e3d-0410-b285-c80daaaae891

--- misc/generate-def.rb
+++ misc/generate-def.rb
@@ -3,7 +3,7 @@
 puts "EXPORTS"
 ARGF.each_line do |line|
   case line
-  when /^\w.+\s+\*?((?:|g|gdk)cut_[a-z_]+)\s*\(/
+  when /^\w.+\s+\*?((?:|g|gdk)cut_[a-z_]+)\s*(?:\(|\z)/
     puts "\t#{$1}"
   end
 end
