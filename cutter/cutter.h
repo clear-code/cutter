8773ee7fbc7355a4638e589826fab304c9132ccb
Kouhei Sutou <kou@cozmixng.org>
2009-05-23 10:02:34 +0000

    * cutter/cutter.h (shutdown): guard shutdown() on Windows.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@2945 10a583ed-1e3d-0410-b285-c80daaaae891

--- cutter/cutter.h
+++ cutter/cutter.h
@@ -213,9 +213,10 @@ void startup(void);
  */
 void cut_startup(void);
 
-#if !defined(CUTTER_DISABLE_DEPRECATED) &&                      \
-    !defined(SHUT_RD) &&                                        \
-    !defined(WINSOCK_API_LINKAGE)
+#if !defined(CUTTER_DISABLE_DEPRECATED) &&      \
+    !defined(SHUT_RD) &&                        \
+    !defined(WINSOCK_API_LINKAGE) &&            \
+    !defined(_WINSOCKAPI_)
 /**
  * shutdown:
  *
