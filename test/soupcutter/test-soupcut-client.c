327096d41c9a0673809ecf37a9bd24d713dd793a
Kouhei Sutou <kou@cozmixng.org>
2009-08-20 13:57:11 +0000

    * test/soupcutter/test-soupcut-client.c: cleanup.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3045 10a583ed-1e3d-0410-b285-c80daaaae891

--- test/soupcutter/test-soupcut-client.c
+++ test/soupcutter/test-soupcut-client.c
@@ -1,88 +1,98 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2009  Yuto HAYAMIZU <y.hayamizu@gmail.com>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <soupcutter/soupcutter.h>
 #include "../lib/cuttest-soup.h"
 
 void test_send_message(void);
 void test_get(void);
-    
-static GError *error;
 
 void
 cut_setup (void)
 {
-    error = NULL;
 }
 
 void
 cut_teardown (void)
 {
-    if (error)
-        g_error_free(error);
-
 }
 
 static void
-server_callback(SoupServer *server,
-                SoupMessage *msg,
-                const gchar *path,
-                GHashTable *query,
-                SoupClientContext *client,
-                gpointer user_data)
+server_callback (SoupServer *server,
+                 SoupMessage *msg,
+                 const gchar *path,
+                 GHashTable *query,
+                 SoupClientContext *client,
+                 gpointer user_data)
 {
     soup_message_set_status(msg, SOUP_STATUS_OK);
     soup_message_set_response(msg, "text/plain", SOUP_MEMORY_COPY,
                               "Hello", 5);
 }
 
-static gpointer
-serve(gpointer data)
+static const gchar *
+serve (SoupCutClient *client)
 {
-    SoupServer *server = data;
+    SoupServer *server;
+    GMainContext *context;
+    const gchar *uri;
+
+    context = soupcut_client_get_async_context(client);
+    server = cuttest_soup_server_take_new(context);
+    uri = cuttest_soup_server_build_uri(server, "/");
 
     soup_server_add_handler(server, "/", server_callback,
                             NULL, NULL);
     soup_server_run_async(server);
 
-    return NULL;
+    return uri;
 }
 
 void
 test_send_message (void)
 {
-    SoupMessage *message;
     SoupCutClient *client;
-    SoupServer *server;
-    GMainContext *context;
+    SoupMessage *message;
     const gchar *uri;
 
     client = soupcut_client_new();
-    context = soupcut_client_get_async_context(client);
-    server = cuttest_soup_server_take_new(context);
-    uri = cuttest_soup_server_build_uri(server, "/");
-    serve(server);
+    uri = serve(client);
 
     message = soup_message_new("GET", uri);
-
-    cut_assert_equal_uint(SOUP_STATUS_OK,
-                          soupcut_client_send_message(client, message));
-
-     cut_assert_equal_string("text/plain", soup_message_headers_get_content_type(message->response_headers, NULL));
+    cut_assert_equal_uint(0, soupcut_client_get_n_messages(client));
+    soupcut_client_send_message(client, message);
     cut_assert_equal_uint(1, soupcut_client_get_n_messages(client));
     gcut_assert_equal_object(message, soupcut_client_get_latest_message(client));
+
+    soupcut_client_assert_response(client);
+    soupcut_client_assert_equal_content_type("text/plain", client);
+    soupcut_client_assert_equal_body("Hello", client);
 }
 
 void
 test_get (void)
 {
     SoupCutClient *client;
-    SoupServer *server;
-    GMainContext *context;
     const gchar *uri;
 
     client = soupcut_client_new();
-    context = soupcut_client_get_async_context(client);
-    server = cuttest_soup_server_take_new(context);
-    uri = cuttest_soup_server_build_uri(server, "/");
-    serve(server);
+    uri = serve(client);
 
     soupcut_client_get(client, uri, NULL);
 
