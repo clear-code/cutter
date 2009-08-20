495bedb111d19779b8b89414d4eea1a70392fadc
Kouhei Sutou <kou@cozmixng.org>
2009-08-20 13:50:18 +0000

    * test/soupcutter/suite-soupcut-test.c: initialize   SOUP_URI_SCHEME_HTTP*.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3044 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/soupcutter/suite-soupcut-test.c
@@ -0,0 +1,32 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2009  Kouhei Sutou <kou@clear-code.com>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libsoup/soup.h>
+
+void soupcut_test_warmup (void);
+
+void
+soupcut_test_warmup (void)
+{
+    SOUP_URI_SCHEME_HTTP;
+    SOUP_URI_SCHEME_HTTPS;
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
