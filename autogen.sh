281841404eca5e110f2131c8c964d07966d30fbc
Kouhei Sutou <kou@cozmixng.org>
2009-08-26 01:29:34 +0000

    * autogen.sh: fix order.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3095 10a583ed-1e3d-0410-b285-c80daaaae891

--- autogen.sh
+++ autogen.sh
@@ -9,10 +9,15 @@ run()
     fi
 }
 
+# for old intltoolize
+if [ ! -e config/po ]; then
+    ln -s ../po config/po
+fi
+
+run ${ACLOCAL:-aclocal} $ACLOCAL_OPTIONS
 run ${LIBTOOLIZE:-libtoolize} --copy --force
 run ${INTLTOOLIZE:-intltoolize} --force --copy
 #run ${GTKDOCIZE:-gtkdocize} --copy
-run ${ACLOCAL:-aclocal} -I m4macros $ACLOCAL_OPTIONS
-run ${AUTOCONF:-autoconf}
 run ${AUTOHEADER:-autoheader}
 run ${AUTOMAKE:-automake} --add-missing --foreign --copy
+run ${AUTOCONF:-autoconf}
