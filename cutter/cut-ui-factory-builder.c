62b5188eda082772d589cfb902439ec2a73fabc8
Kouhei Sutou <kou@cozmixng.org>
2009-10-01 09:31:13 +0000

    * cutter/cut-ui-factory-builder.c: remove needless const.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3189 10a583ed-1e3d-0410-b285-c80daaaae891

--- cutter/cut-ui-factory-builder.c
+++ cutter/cut-ui-factory-builder.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
- *  Copyright (C) 2007  Kouhei Sutou <kou@cozmixng.org>
+ *  Copyright (C) 2007-2009  Kouhei Sutou <kou@clear-code.com>
  *
  *  This library is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -29,7 +29,7 @@
 #include "cut-module-factory.h"
 #include "cut-module-factory-utils.h"
 
-static const gchar *ui_name = NULL;
+static gchar *ui_name = NULL;
 static CutUIFactoryBuilder *the_builder = NULL;
 #ifdef G_OS_WIN32
 static gchar *win32_ui_factory_module_dir = NULL;
