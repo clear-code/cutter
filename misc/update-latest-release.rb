3979869963068b75261cf05bd2ba82f466f79c64
Kouhei Sutou <kou@cozmixng.org>
2009-10-28 08:16:40 +0000

    * misc/update-latest-release.rb: add for automating latest release   version/date updating.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3291 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ misc/update-latest-release.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'time'
+
+if ARGV.size < 4
+  puts "Usage: #{$0} " +
+         "OLD_VERSION OLD_RELEASE_DATE " +
+         "NEW_VERSION NEW_RELEASE_DATE " +
+         "FILES..."
+  puts " e.g.: #{$0} 1.0.9 2009-10-20 1.1.0 2009-11-29 " +
+         " html/index.html html/index.html.ja rpm/fedora/cutter.spec.in"
+  exit(1)
+end
+
+old_version, old_release_date, new_version, new_release_date, *files = ARGV
+
+files.each do |file|
+  content = replaced_content = File.read(file)
+  case file
+  when /\.spec(?:\.in)?\z/
+    date = Time.parse(new_release_date).strftime("%a %b %d %Y")
+    if content !~ /#{Regexp.escape(new_version)}/
+      replaced_content = content.sub(/^(%changelog\n)/, <<-EOC)
+%changelog
+* #{date} Kouhei Sutou <kou@clear-code.com>
+- (#{new_version}-0)
+- new upstream release
+
+      EOC
+    end
+  else
+    [[old_version, new_version],
+     [old_release_date, new_release_date]].each do |old, new|
+      replaced_content = replaced_content.gsub(/#{Regexp.escape(old)}/, new)
+    end
+  end
+
+  next if replaced_content == content
+  File.open(file, "w") do |output|
+    output.print(replaced_content)
+  end
+end
