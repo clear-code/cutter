4383956c0ed363162b2f42aefba6495732eb07b6
Kouhei Sutou <kou@cozmixng.org>
2009-11-09 13:28:11 +0000

    * misc/generate-makefile-objects.rb: specialize enum-types C source.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3331 10a583ed-1e3d-0410-b285-c80daaaae891

--- misc/generate-makefile-objects.rb
+++ misc/generate-makefile-objects.rb
@@ -3,6 +3,11 @@
 puts "#{ARGV.shift} = \\\n"
 prefix = ARGV.shift
 files = ARGV.collect do |file|
-  "\t#{prefix}#{file.sub(/\.c/, '.obj')}"
+  _prefix = prefix
+  case file
+  when /-enum-types/
+    _prefix = _prefix.sub(/(\$\(top_builddir\))/, '\1/vcc/source')
+  end
+  "\t#{_prefix}#{file.sub(/\.c/, '.obj')}"
 end.join(" \\\n")
 puts files
