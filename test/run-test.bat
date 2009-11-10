1f93b794a71e54a812716131052cbcaa6a118069
Kouhei Sutou <kou@cozmixng.org>
2009-11-10 12:54:35 +0000

    * test/run-test.bat: add.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3347 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/run-test.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set BASE_DIR=%~p0
+set TOP_DIR=%BASE_DIR%..
+
+cd %TOP_DIR%
+nmake -nologo -f Makefile.win32
+
+set PATH=%PATH%;%TOP_DIR%\vcc\vendor\bin
+
+set CUTTER=%TOP_DIR%\cutter\cutter.exe
+
+set CUT_UI_MODULE_DIR=%TOP_DIR%\module\ui
+set CUT_UI_FACTORY_MODULE_DIR=%TOP_DIR%\module\ui
+set CUT_REPORT_MODULE_DIR=%TOP_DIR%\module\report
+set CUT_REPORT_FACTORY_MODULE_DIR=%TOP_DIR%\module\report
+set CUT_STREAM_MODULE_DIR=%TOP_DIR%\module\stream
+set CUT_STREAM_FACTORY_MODULE_DIR=%TOP_DIR%\module\stream
+
+@echo on
+
+%CUTTER% -s %BASE_DIR% --exclude-directory fixtures --exclude-directory lib %BASE_DIR%
