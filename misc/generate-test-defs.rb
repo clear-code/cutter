06fe70775c31befd190fce77a648f6c186d9c7f1
Kouhei Sutou <kou@cozmixng.org>
2009-11-14 08:38:52 +0000

    * misc/generate-test-defs.rb, misc/generate-test-makefile.rb,   vcc/Makefile.am, test/fixture/**/Makefile.win32: start supporting   test fixture build.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3396 10a583ed-1e3d-0410-b285-c80daaaae891

--- misc/generate-test-defs.rb
+++ misc/generate-test-defs.rb
@@ -10,14 +10,17 @@ FileUtils.mkdir_p(output_dir)
 ARGV.each do |file|
   base = file.sub(/\.la$/, '')
   File.open(File.join(output_dir, "#{base}.def"), "w") do |def_file|
-    content = File.read(File.join(source_dir, "#{base}.c"))
-    def_file.puts("EXPORTS")
-    content.scan(/^void\s+([a-z_]+)\s*\(/) do
-      function = $1
-      case function
-      when /u?int(max|ptr)/
-      else
-        def_file.puts("\t#{function}")
+    c_source = File.join(source_dir, "#{base}.c")
+    if File.exist?(c_source)
+      content = File.read(c_source)
+      def_file.puts("EXPORTS")
+      content.scan(/^void\s+([a-z_]+)\s*\(/) do
+        function = $1
+        case function
+        when /u?int(max|ptr)/
+        else
+          def_file.puts("\t#{function}")
+        end
       end
     end
   end
--- misc/generate-test-makefile.rb
+++ misc/generate-test-makefile.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 
-resource_dir = ARGV.shift
+top_srcdir = ARGV.shift
+target_path = ARGV.shift
+
+resource_dir = "#{top_srcdir}\\vcc\\resource\\#{target_path.gsub(/\//, '\\')}"
+makefile_path = "Makefile.#{target_path.split(/\//).join('.')}"
 
 test_bases = ARGV.collect do |file|
   file.sub(/\.la$/, '')
@@ -9,17 +13,20 @@ end
 test_dlls = test_bases.collect do |base|
   "#{base}.dll"
 end
-puts (["tests = "] + test_dlls).join(" \\\n\t")
-puts
 
-test_bases.each do |base|
-  dll = "#{base}.dll"
-  obj = "#{base}.obj"
-  local_cflags = base.gsub(/-/, "_") + "_CFLAGS"
-  local_ldflags = base.gsub(/-/, "_") + "_LDFLAGS"
-  puts("#{dll}: #{obj}")
-  puts("\t$(CC) $(CFLAGS) $(#{local_cflags}) -LD -Fe$@ #{obj} " +
-       "$(LIBS) $(LDFLAGS) $(#{local_ldflags}) " +
-       "/def:#{resource_dir}\\#{base}.def")
-  puts
+File.open(makefile_path, "w") do |makefile|
+  makefile.puts((["tests = "] + test_dlls).join(" \\\n\t"))
+  makefile.puts
+
+  test_bases.each do |base|
+    dll = "#{base}.dll"
+    obj = "#{base}.obj"
+    local_cflags = base.gsub(/-/, "_") + "_CFLAGS"
+    local_ldflags = base.gsub(/-/, "_") + "_LDFLAGS"
+    makefile.puts("#{dll}: #{obj}")
+    makefile.puts("\t$(CC) $(CFLAGS) $(#{local_cflags}) -LD -Fe$@ #{obj} " +
+                  "$(LIBS) $(LDFLAGS) $(#{local_ldflags}) " +
+                  "/def:#{resource_dir}\\#{base}.def")
+    makefile.puts
+  end
 end
