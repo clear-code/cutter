63b6b199653f04088b6442520bf2ebd561394d82
Kouhei Sutou <kou@cozmixng.org>
2009-11-14 08:44:06 +0000

    * vcc/: sync.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3397 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ vcc/Makefile.test.fixtures.loader.cpp
@@ -0,0 +1,6 @@
+tests =  \
+	stub-namespace.dll
+
+stub-namespace.dll: stub-namespace.obj
+	$(CC) $(CFLAGS) $(stub_namespace_CFLAGS) -LD -Fe$@ stub-namespace.obj $(LIBS) $(LDFLAGS) $(stub_namespace_LDFLAGS) /def:$(top_srcdir)\vcc\resource\test\fixtures\loader\cpp\stub-namespace.def
+
