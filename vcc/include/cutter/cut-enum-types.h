cd0a2e69c3749bacbfc2e2bc6993f0ef592a3d1a
Kouhei Sutou <kou@cozmixng.org>
2009-11-14 02:23:34 +0000

    * vcc/: sync.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3355 10a583ed-1e3d-0410-b285-c80daaaae891

--- vcc/include/cutter/cut-enum-types.h
+++ vcc/include/cutter/cut-enum-types.h
@@ -10,24 +10,24 @@ G_BEGIN_DECLS
 /* enumerations from "cut-public.h" */
 GType cut_test_result_status_get_type (void);
 #define CUT_TYPE_TEST_RESULT_STATUS (cut_test_result_status_get_type())
-GType cut_order_get_type (void);
-#define CUT_TYPE_ORDER (cut_order_get_type())
-GType cut_test_context_error_get_type (void);
-#define CUT_TYPE_TEST_CONTEXT_ERROR (cut_test_context_error_get_type())
+GType cut_diff_writer_tag_get_type (void);
+#define CUT_TYPE_DIFF_WRITER_TAG (cut_diff_writer_tag_get_type())
+GType cut_file_stream_reader_error_get_type (void);
+#define CUT_TYPE_FILE_STREAM_READER_ERROR (cut_file_stream_reader_error_get_type())
 GType cut_pipeline_error_get_type (void);
 #define CUT_TYPE_PIPELINE_ERROR (cut_pipeline_error_get_type())
+GType cut_order_get_type (void);
+#define CUT_TYPE_ORDER (cut_order_get_type())
+GType cut_sequence_match_operation_type_get_type (void);
+#define CUT_TYPE_SEQUENCE_MATCH_OPERATION_TYPE (cut_sequence_match_operation_type_get_type())
+GType cut_stream_reader_error_get_type (void);
+#define CUT_TYPE_STREAM_READER_ERROR (cut_stream_reader_error_get_type())
+GType cut_test_context_error_get_type (void);
+#define CUT_TYPE_TEST_CONTEXT_ERROR (cut_test_context_error_get_type())
 GType cut_verbose_level_get_type (void);
 #define CUT_TYPE_VERBOSE_LEVEL (cut_verbose_level_get_type())
 GType cut_verbose_level_error_get_type (void);
 #define CUT_TYPE_VERBOSE_LEVEL_ERROR (cut_verbose_level_error_get_type())
-GType cut_sequence_match_operation_type_get_type (void);
-#define CUT_TYPE_SEQUENCE_MATCH_OPERATION_TYPE (cut_sequence_match_operation_type_get_type())
-GType cut_diff_writer_tag_get_type (void);
-#define CUT_TYPE_DIFF_WRITER_TAG (cut_diff_writer_tag_get_type())
-GType cut_stream_reader_error_get_type (void);
-#define CUT_TYPE_STREAM_READER_ERROR (cut_stream_reader_error_get_type())
-GType cut_file_stream_reader_error_get_type (void);
-#define CUT_TYPE_FILE_STREAM_READER_ERROR (cut_file_stream_reader_error_get_type())
 G_END_DECLS
 
 #endif /* __CUT_ENUM_TYPES_H__ */
--- vcc/source/cutter/cut-enum-types.c
+++ vcc/source/cutter/cut-enum-types.c
@@ -3,51 +3,51 @@
 
 #include "cut-enum-types.h"
 #include "cut-public.h"
-#include "cut-private.h"
-#include "cut-runner.h"
-#include "cut-run-context.h"
-#include "cut-test-runner.h"
-#include "cut-listener.h"
-#include "cut-listener-utils.h"
-#include "cut-main.h"
-#include "cut-test.h"
-#include "cut-iterated-test.h"
-#include "cut-test-container.h"
-#include "cut-test-iterator.h"
-#include "cut-test-case.h"
-#include "cut-test-suite.h"
-#include "cut-test-context.h"
-#include "cut-test-result.h"
-#include "cut-test-data.h"
-#include "cut-ui.h"
-#include "cut-contractor.h"
-#include "cut-factory-builder.h"
-#include "cut-report-factory-builder.h"
-#include "cut-stream-factory-builder.h"
-#include "cut-ui-factory-builder.h"
-#include "cut-module-factory.h"
-#include "cut-module-factory-utils.h"
-#include "cut-report.h"
-#include "cut-stream.h"
-#include "cut-process.h"
-#include "cut-pipeline.h"
-#include "cut-stream-parser.h"
-#include "cut-verbose-level.h"
-#include "cut-sequence-matcher.h"
-#include "cut-differ.h"
-#include "cut-diff-writer.h"
-#include "cut-string-diff-writer.h"
-#include "cut-console-diff-writer.h"
-#include "cut-readable-differ.h"
-#include "cut-unified-differ.h"
-#include "cut-colorize-differ.h"
-#include "cut-console.h"
 #include "cut-analyzer.h"
-#include "cut-stream-reader.h"
-#include "cut-file-stream-reader.h"
-#include "cut-sub-process.h"
-#include "cut-sub-process-group.h"
 #include "cut-backtrace-entry.h"
+#include "cut-colorize-differ.h"
+#include "cut-console-diff-writer.h"
+#include "cut-console.h"
+#include "cut-contractor.h"
+#include "cut-diff-writer.h"
+#include "cut-differ.h"
+#include "cut-factory-builder.h"
+#include "cut-file-stream-reader.h"
+#include "cut-iterated-test.h"
+#include "cut-listener-utils.h"
+#include "cut-listener.h"
+#include "cut-main.h"
+#include "cut-module-factory-utils.h"
+#include "cut-module-factory.h"
+#include "cut-pipeline.h"
+#include "cut-private.h"
+#include "cut-process.h"
+#include "cut-readable-differ.h"
+#include "cut-report-factory-builder.h"
+#include "cut-report.h"
+#include "cut-run-context.h"
+#include "cut-runner.h"
+#include "cut-sequence-matcher.h"
+#include "cut-stream-factory-builder.h"
+#include "cut-stream-parser.h"
+#include "cut-stream-reader.h"
+#include "cut-stream.h"
+#include "cut-string-diff-writer.h"
+#include "cut-sub-process-group.h"
+#include "cut-sub-process.h"
+#include "cut-test-case.h"
+#include "cut-test-container.h"
+#include "cut-test-context.h"
+#include "cut-test-data.h"
+#include "cut-test-iterator.h"
+#include "cut-test-result.h"
+#include "cut-test-runner.h"
+#include "cut-test-suite.h"
+#include "cut-test.h"
+#include "cut-ui-factory-builder.h"
+#include "cut-ui.h"
+#include "cut-unified-differ.h"
+#include "cut-verbose-level.h"
 
 /* enumerations from "cut-public.h" */
 GType
@@ -68,96 +68,6 @@ cut_test_result_status_get_type (void)
       { 0, NULL, NULL }
     };
     etype = g_enum_register_static ("CutTestResultStatus", values);
-  }
-  return etype;
-}
-GType
-cut_order_get_type (void)
-{
-  static GType etype = 0;
-  if (etype == 0) {
-    static const GEnumValue values[] = {
-      { CUT_ORDER_NONE_SPECIFIED, "CUT_ORDER_NONE_SPECIFIED", "none-specified" },
-      { CUT_ORDER_NAME_ASCENDING, "CUT_ORDER_NAME_ASCENDING", "name-ascending" },
-      { CUT_ORDER_NAME_DESCENDING, "CUT_ORDER_NAME_DESCENDING", "name-descending" },
-      { 0, NULL, NULL }
-    };
-    etype = g_enum_register_static ("CutOrder", values);
-  }
-  return etype;
-}
-GType
-cut_test_context_error_get_type (void)
-{
-  static GType etype = 0;
-  if (etype == 0) {
-    static const GEnumValue values[] = {
-      { CUT_TEST_CONTEXT_ERROR_FATAL, "CUT_TEST_CONTEXT_ERROR_FATAL", "fatal" },
-      { 0, NULL, NULL }
-    };
-    etype = g_enum_register_static ("CutTestContextError", values);
-  }
-  return etype;
-}
-GType
-cut_pipeline_error_get_type (void)
-{
-  static GType etype = 0;
-  if (etype == 0) {
-    static const GEnumValue values[] = {
-      { CUT_PIPELINE_ERROR_PIPE, "CUT_PIPELINE_ERROR_PIPE", "pipe" },
-      { CUT_PIPELINE_ERROR_COMMAND_LINE, "CUT_PIPELINE_ERROR_COMMAND_LINE", "command-line" },
-      { CUT_PIPELINE_ERROR_SPAWN, "CUT_PIPELINE_ERROR_SPAWN", "spawn" },
-      { CUT_PIPELINE_ERROR_CHILD_PID, "CUT_PIPELINE_ERROR_CHILD_PID", "child-pid" },
-      { CUT_PIPELINE_ERROR_IO_ERROR, "CUT_PIPELINE_ERROR_IO_ERROR", "io-error" },
-      { 0, NULL, NULL }
-    };
-    etype = g_enum_register_static ("CutPipelineError", values);
-  }
-  return etype;
-}
-GType
-cut_verbose_level_get_type (void)
-{
-  static GType etype = 0;
-  if (etype == 0) {
-    static const GEnumValue values[] = {
-      { CUT_VERBOSE_LEVEL_INVALID, "CUT_VERBOSE_LEVEL_INVALID", "invalid" },
-      { CUT_VERBOSE_LEVEL_SILENT, "CUT_VERBOSE_LEVEL_SILENT", "silent" },
-      { CUT_VERBOSE_LEVEL_NORMAL, "CUT_VERBOSE_LEVEL_NORMAL", "normal" },
-      { CUT_VERBOSE_LEVEL_VERBOSE, "CUT_VERBOSE_LEVEL_VERBOSE", "verbose" },
-      { 0, NULL, NULL }
-    };
-    etype = g_enum_register_static ("CutVerboseLevel", values);
-  }
-  return etype;
-}
-GType
-cut_verbose_level_error_get_type (void)
-{
-  static GType etype = 0;
-  if (etype == 0) {
-    static const GEnumValue values[] = {
-      { CUT_VERBOSE_LEVEL_ERROR_BAD_VALUE, "CUT_VERBOSE_LEVEL_ERROR_BAD_VALUE", "value" },
-      { 0, NULL, NULL }
-    };
-    etype = g_enum_register_static ("CutVerboseLevelError", values);
-  }
-  return etype;
-}
-GType
-cut_sequence_match_operation_type_get_type (void)
-{
-  static GType etype = 0;
-  if (etype == 0) {
-    static const GEnumValue values[] = {
-      { CUT_SEQUENCE_MATCH_OPERATION_EQUAL, "CUT_SEQUENCE_MATCH_OPERATION_EQUAL", "equal" },
-      { CUT_SEQUENCE_MATCH_OPERATION_INSERT, "CUT_SEQUENCE_MATCH_OPERATION_INSERT", "insert" },
-      { CUT_SEQUENCE_MATCH_OPERATION_DELETE, "CUT_SEQUENCE_MATCH_OPERATION_DELETE", "delete" },
-      { CUT_SEQUENCE_MATCH_OPERATION_REPLACE, "CUT_SEQUENCE_MATCH_OPERATION_REPLACE", "replace" },
-      { 0, NULL, NULL }
-    };
-    etype = g_enum_register_static ("CutSequenceMatchOperationType", values);
   }
   return etype;
 }
@@ -189,6 +99,67 @@ cut_diff_writer_tag_get_type (void)
   return etype;
 }
 GType
+cut_file_stream_reader_error_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { CUT_FILE_STREAM_READER_ERROR_FILE, "CUT_FILE_STREAM_READER_ERROR_FILE", "file" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static ("CutFileStreamReaderError", values);
+  }
+  return etype;
+}
+GType
+cut_pipeline_error_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { CUT_PIPELINE_ERROR_PIPE, "CUT_PIPELINE_ERROR_PIPE", "pipe" },
+      { CUT_PIPELINE_ERROR_COMMAND_LINE, "CUT_PIPELINE_ERROR_COMMAND_LINE", "command-line" },
+      { CUT_PIPELINE_ERROR_SPAWN, "CUT_PIPELINE_ERROR_SPAWN", "spawn" },
+      { CUT_PIPELINE_ERROR_CHILD_PID, "CUT_PIPELINE_ERROR_CHILD_PID", "child-pid" },
+      { CUT_PIPELINE_ERROR_IO_ERROR, "CUT_PIPELINE_ERROR_IO_ERROR", "io-error" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static ("CutPipelineError", values);
+  }
+  return etype;
+}
+GType
+cut_order_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { CUT_ORDER_NONE_SPECIFIED, "CUT_ORDER_NONE_SPECIFIED", "none-specified" },
+      { CUT_ORDER_NAME_ASCENDING, "CUT_ORDER_NAME_ASCENDING", "name-ascending" },
+      { CUT_ORDER_NAME_DESCENDING, "CUT_ORDER_NAME_DESCENDING", "name-descending" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static ("CutOrder", values);
+  }
+  return etype;
+}
+GType
+cut_sequence_match_operation_type_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { CUT_SEQUENCE_MATCH_OPERATION_EQUAL, "CUT_SEQUENCE_MATCH_OPERATION_EQUAL", "equal" },
+      { CUT_SEQUENCE_MATCH_OPERATION_INSERT, "CUT_SEQUENCE_MATCH_OPERATION_INSERT", "insert" },
+      { CUT_SEQUENCE_MATCH_OPERATION_DELETE, "CUT_SEQUENCE_MATCH_OPERATION_DELETE", "delete" },
+      { CUT_SEQUENCE_MATCH_OPERATION_REPLACE, "CUT_SEQUENCE_MATCH_OPERATION_REPLACE", "replace" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static ("CutSequenceMatchOperationType", values);
+  }
+  return etype;
+}
+GType
 cut_stream_reader_error_get_type (void)
 {
   static GType etype = 0;
@@ -205,15 +176,44 @@ cut_stream_reader_error_get_type (void)
   return etype;
 }
 GType
-cut_file_stream_reader_error_get_type (void)
+cut_test_context_error_get_type (void)
 {
   static GType etype = 0;
   if (etype == 0) {
     static const GEnumValue values[] = {
-      { CUT_FILE_STREAM_READER_ERROR_FILE, "CUT_FILE_STREAM_READER_ERROR_FILE", "file" },
+      { CUT_TEST_CONTEXT_ERROR_FATAL, "CUT_TEST_CONTEXT_ERROR_FATAL", "fatal" },
       { 0, NULL, NULL }
     };
-    etype = g_enum_register_static ("CutFileStreamReaderError", values);
+    etype = g_enum_register_static ("CutTestContextError", values);
+  }
+  return etype;
+}
+GType
+cut_verbose_level_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { CUT_VERBOSE_LEVEL_INVALID, "CUT_VERBOSE_LEVEL_INVALID", "invalid" },
+      { CUT_VERBOSE_LEVEL_SILENT, "CUT_VERBOSE_LEVEL_SILENT", "silent" },
+      { CUT_VERBOSE_LEVEL_NORMAL, "CUT_VERBOSE_LEVEL_NORMAL", "normal" },
+      { CUT_VERBOSE_LEVEL_VERBOSE, "CUT_VERBOSE_LEVEL_VERBOSE", "verbose" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static ("CutVerboseLevel", values);
+  }
+  return etype;
+}
+GType
+cut_verbose_level_error_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { CUT_VERBOSE_LEVEL_ERROR_BAD_VALUE, "CUT_VERBOSE_LEVEL_ERROR_BAD_VALUE", "value" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static ("CutVerboseLevelError", values);
   }
   return etype;
 }
