26ac289179ae08a0ed5b863530e88f16b94a7ed0
Kouhei Sutou <kou@cozmixng.org>
2009-11-15 13:42:18 +0000

    * cppcutter/cppcut-macros.h: add.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3415 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ cppcutter/cppcut-macros.h
@@ -0,0 +1,34 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2009  Kouhei Sutou <kou@clear-code.com>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __CPPCUT_MACROS_H__
+#define __CPPCUT_MACROS_H__
+
+
+#ifdef CPPCUT_BUILDING
+#  define CPPCUT_DECL __declspec(dllexport)
+#else
+#  define CPPCUT_DECL __declspec(dllimport)
+#endif
+
+#endif /* __CPPCUT_MACROS_H__ */
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
