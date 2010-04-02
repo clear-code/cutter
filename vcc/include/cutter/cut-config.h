ba4f330badf87dc8bb88ed7696208112e51b9949
Kouhei Sutou <kou@cozmixng.org>
2010-04-02 07:05:06 +0000

    * vcc/include/cutter/cut-config.h: add.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3497 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ vcc/include/cutter/cut-config.h
@@ -0,0 +1,15 @@
+/* cut-config.h
+ *
+ * This is a generated file.  Please modify 'configure.ac'
+ */
+
+#ifndef __CUT_CONFIG_H__
+#define __CUT_CONFIG_H__
+
+#define CUT_HAVE_STDINT_H 1
+/* #define CUT_HAVE_INTTYPES_H 1 */
+#define CUT_HAVE_WINSOCK2_H
+/* #define CUT_HAVE_SYS_SOCKET_H 1 */
+/* #define CUT_HAVE_SYS_UN_H 1 */
+
+#endif
