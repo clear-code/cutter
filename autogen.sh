164cf7ea73f6af6ccada0910bd7cc02493a558ee
Kouhei Sutou <kou@cozmixng.org>
2010-04-14 07:56:23 +0000

    * autogen.sh: use suitable default value for each environment.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3612 10a583ed-1e3d-0410-b285-c80daaaae891

--- autogen.sh
+++ autogen.sh
@@ -14,6 +14,15 @@ if [ ! -e config/po ]; then
     ln -s ../po config/po
 fi
 
+case `uname -s` in
+    Darwin)
+	: ${LIBTOOLIZE:-glibtoolize}
+	;;
+    FreeBSD)
+	: ${ACLOCAL_OPTIONS:"$ACLOCAL_OPTIONS -I /usr/local/share/aclocal/"}
+	;;
+esac
+
 run ${ACLOCAL:-aclocal} -I acmacros -I m4macros $ACLOCAL_OPTIONS
 run ${LIBTOOLIZE:-libtoolize} --copy --force
 run ${INTLTOOLIZE:-intltoolize} --force --copy
