c89541e89e46e432e870316ce5e8337ac207e90e
Kouhei Sutou <kou@cozmixng.org>
2010-06-10 02:42:58 +0000

    * yum/Makefile.am, yum/build-repository-rpm.sh, yum/cutter.repo:   create repository RPM for each distribution.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3681 10a583ed-1e3d-0410-b285-c80daaaae891

--- yum/build-repository-rpm.sh
+++ yum/build-repository-rpm.sh
@@ -2,6 +2,15 @@
 
 script_base_dir=`dirname $0`
 
+if [ $# != 2 ]; then
+    echo "Usage: $0 PACKAGE DISTRIBUTIONS"
+    echo " e.g.: $0 cutter 'fedora centos'"
+    exit 1
+fi
+
+PACKAGE=$1
+DISTRIBUTIONS=$2
+
 run()
 {
     "$@"
@@ -11,7 +20,6 @@ run()
     fi
 }
 
-PACKAGE=cutter
 rpm_base_dir=$HOME/rpm
 
 if [ ! -f ~/.rpmmacros ]; then
@@ -26,16 +34,36 @@ run mkdir -p $rpm_base_dir/BUILD
 run mkdir -p $rpm_base_dir/RPMS
 run mkdir -p $rpm_base_dir/SRPMS
 
-run tar cfz $rpm_base_dir/SOURCES/${PACKAGE}-repository.tar.gz \
-  -C ${script_base_dir} ${PACKAGE}.repo RPM-GPG-KEY-${PACKAGE}
-run cp ${script_base_dir}/${PACKAGE}-repository.spec $rpm_base_dir/SPECS/
+for distribution in ${DISTRIBUTIONS}; do
+    case $distribution in
+	fedora)
+	    distribution_label=Fedora
+	    ;;
+	centos)
+	    distribution_label=CentOS
+	    ;;
+    esac
+    repo=${PACKAGE}.repo
+    run cat <<EOR > $repo
+[cutter]
+name=Cutter for $distribution_label \$releasever - \$basearch
+baseurl=http://cutter.sourceforge.net/$distribution/\$releasever/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-cutter
+metadata_expire=7d
+EOR
+    run tar cfz $rpm_base_dir/SOURCES/${PACKAGE}-repository.tar.gz \
+	-C ${script_base_dir} ${repo} RPM-GPG-KEY-${PACKAGE}
+    run cp ${script_base_dir}/${PACKAGE}-repository.spec $rpm_base_dir/SPECS/
 
-run rpmbuild -ba $rpm_base_dir/SPECS/${PACKAGE}-repository.spec
+    run rpmbuild -ba $rpm_base_dir/SPECS/${PACKAGE}-repository.spec
 
-top_dir=$script_base_dir/fedora
+    top_dir=$script_base_dir/$distribution
 
-run mkdir -p $top_dir
-run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-repository-* $top_dir
-run cp -p $rpm_base_dir/SRPMS/${PACKAGE}-repository-* $top_dir
+    run mkdir -p $top_dir
+    run cp -p $rpm_base_dir/RPMS/noarch/${PACKAGE}-repository-* $top_dir
+    run cp -p $rpm_base_dir/SRPMS/${PACKAGE}-repository-* $top_dir
 
-run cp -p ${script_base_dir}/RPM-GPG-KEY-${PACKAGE} $top_dir
+    run cp -p ${script_base_dir}/RPM-GPG-KEY-${PACKAGE} $top_dir
+done
