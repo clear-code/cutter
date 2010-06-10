1919b0d0d09b4da0c8606301d2cb621f6bc1f1c6
Kouhei Sutou <kou@cozmixng.org>
2010-06-10 02:33:20 +0000

    * yum/: support multi distribution packaging.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3680 10a583ed-1e3d-0410-b285-c80daaaae891

--- yum/sign-rpm.sh
+++ yum/sign-rpm.sh
@@ -1,6 +1,14 @@
-#!/bin/zsh
+#!/bin/sh
 
 script_base_dir=`dirname $0`
+
+if [ $# != 1 ]; then
+    echo "Usage: $0 DISTRIBUTIONS"
+    echo " e.g.: $0 'fedora centos'"
+    exit 1
+fi
+
+DISTRIBUTIONS=$1
 
 run()
 {
@@ -11,6 +19,8 @@ run()
     fi
 }
 
-for rpm in $script_base_dir/fedora/12/*/*/*.rpm; do
-    run rpm -D "_gpg_name `$script_base_dir/gpg-uid.sh`" --resign $rpm
+for distribution in ${DISTRIBUTIONS}; do
+    for rpm in $script_base_dir/${distribution}/*/*/*/*.rpm; do
+	run rpm -D "_gpg_name `$script_base_dir/gpg-uid.sh`" --resign $rpm
+    done;
 done
--- yum/update-repository.sh
+++ yum/update-repository.sh
@@ -2,6 +2,14 @@
 
 script_base_dir=`dirname $0`
 
+if [ $# != 1 ]; then
+    echo "Usage: $0 DISTRIBUTIONS"
+    echo " e.g.: $0 'fedora centos'"
+    exit 1
+fi
+
+DISTRIBUTIONS=$1
+
 run()
 {
     "$@"
@@ -11,9 +19,11 @@ run()
     fi
 }
 
-for dir in $script_base_dir/fedora/12/*; do
-    run createrepo $dir
-done
+for distribution in ${DISTRIBUTIONS}; do
+    for dir in $script_base_dir/${distribution}/*/*; do
+	run createrepo $dir
+    done;
 
-run $script_base_dir/gpg-public-key.sh > \
-    $script_base_dir/fedora/RPM-GPG-KEY-cutter
+    run $script_base_dir/gpg-public-key.sh > \
+	$script_base_dir/${distribution}/RPM-GPG-KEY-cutter;
+done
