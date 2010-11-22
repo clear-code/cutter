9ecf75e9dd976f6db904e5140328e93778dea54a
Kouhei Sutou <kou@cozmixng.org>
2010-11-22 15:11:32 +0000

    * Makefile.am, misc/update-latest-release.rb: parameterize package name.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3826 10a583ed-1e3d-0410-b285-c80daaaae891

--- misc/update-latest-release.rb
+++ misc/update-latest-release.rb
@@ -4,15 +4,17 @@ require 'time'
 
 if ARGV.size < 4
   puts "Usage: #{$0} " +
+         "PACKAGE " +
          "OLD_VERSION OLD_RELEASE_DATE " +
          "NEW_VERSION NEW_RELEASE_DATE " +
          "FILES..."
-  puts " e.g.: #{$0} 1.0.9 2009-10-20 1.1.0 2009-11-29 " +
+  puts " e.g.: #{$0} cutter 1.0.9 2009-10-20 1.1.0 2009-11-29 " +
          " html/index.html html/index.html.ja rpm/fedora/cutter.spec.in"
   exit(1)
 end
 
-old_version, old_release_date, new_version, new_release_date, *files = ARGV
+package, old_version, old_release_date, new_version, new_release_date, *files =
+  ARGV
 
 files.each do |file|
   content = replaced_content = File.read(file)
@@ -32,7 +34,7 @@ files.each do |file|
     date = Time.parse(new_release_date).rfc2822
     if content !~ /#{Regexp.escape(new_version)}/
       replaced_content = content.sub(/\A/, <<-EOC)
-cutter (#{new_version}-1) unstable; urgency=low
+#{package} (#{new_version}-1) unstable; urgency=low
 
   * New upstream release.
 
