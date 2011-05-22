2641f82abbd421dd5e113910d7767951a3efd608
Kouhei Sutou <kou@cozmixng.org>
2011-05-22 04:53:11 +0000

    * cutter/cut-stream-factory-builder.c: suppress warnings.
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3958 10a583ed-1e3d-0410-b285-c80daaaae891

--- cutter/cut-stream-factory-builder.c
+++ cutter/cut-stream-factory-builder.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
- *  Copyright (C) 2007-2009  Kouhei Sutou <kou@cozmixng.org>
+ *  Copyright (C) 2007-2011  Kouhei Sutou <kou@clear-code.com>
  *
  *  This library is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -212,13 +212,11 @@ static GList *
 build (CutFactoryBuilder *builder)
 {
     GList *factories = NULL;
-    CutStreamFactoryBuilderPrivate *priv;
     CutModuleFactory *factory;
 
     if (!stream_name)
         return NULL;
 
-    priv = CUT_STREAM_FACTORY_BUILDER_GET_PRIVATE(builder);
     factory = build_factory(builder, stream_name,
                             "fd", fd,
                             "directory", directory,
