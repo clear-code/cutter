02fc17b8713cfb45431435184273e1930958d07d
Kouhei Sutou <kou@cozmixng.org>
2011-07-03 12:42:30 +0000

    * test/cutter/test-cut-thread.c,   test/fixtures/thread/failure/stub-failure.c: add a test for   cut_fail() in thread. It's not resolved yet. Bug #3341429   Reported by Romain Tartière. Thanks!!!
    git-svn-id: https://cutter.svn.sourceforge.net/svnroot/cutter/cutter/trunk@3963 10a583ed-1e3d-0410-b285-c80daaaae891

---
+++ test/cutter/test-cut-thread.c
@@ -0,0 +1,73 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2011  Kouhei Sutou <kou@clear-code.com>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <gcutter.h>
+#include <cutter/cut-pipeline.h>
+
+#include "../lib/cuttest-assertions.h"
+
+void test_failure (void);
+
+static CutRunContext *pipeline;
+
+void
+cut_setup (void)
+{
+    pipeline = cut_pipeline_new();
+}
+
+void
+cut_teardown (void)
+{
+    if (pipeline)
+        g_object_unref(pipeline);
+}
+
+static void
+report_error (CutRunContext *context, GError *error, gpointer user_data)
+{
+    gcut_assert_error(error, cut_message("Pipeline Error"));
+}
+
+static gboolean
+run (const gchar *test_dir)
+{
+    cut_run_context_set_test_directory(pipeline, test_dir);
+    cut_run_context_set_source_directory(pipeline, test_dir);
+
+    g_signal_connect(pipeline, "error", G_CALLBACK(report_error), NULL);
+
+    return cut_run_context_start(pipeline);
+}
+
+void
+test_failure_not_crashed (void)
+{
+    cut_omit("cut_fail() in thread isn't supported yet.");
+    cut_assert_false(run(cut_build_path(cuttest_get_base_dir(),
+                                        "fixtures",
+                                        "thread",
+                                        "failure",
+                                        NULL)));
+    cut_assert_false(cut_run_context_is_crashed(pipeline));
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
---
+++ test/fixtures/thread/failure/stub-failure.c
@@ -0,0 +1,61 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2011  Kouhei Sutou <kou@clear-code.com>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <gcutter.h>
+
+void test_failure (void);
+
+static gpointer
+fail (gpointer data)
+{
+    CutTestContext *context = data;
+    gint i;
+
+    cut_set_current_test_context(context);
+
+    for (i = 0; i < 10; i++) {
+        cut_fail("failed in thread");
+    }
+
+    return NULL;
+}
+
+void
+test_failure (void)
+{
+    GThread *threads[64];
+    gsize i;
+
+    for (i = 0; i < sizeof(threads) / sizeof(*threads); i++) {
+        GError *error = NULL;
+        threads[i] = g_thread_create(fail,
+                                     cut_get_current_test_context(),
+                                     TRUE,
+                                     &error);
+        gcut_assert_error(error);
+    }
+
+    for (i = 0; i < sizeof(threads) / sizeof(*threads); i++) {
+	g_thread_join(threads[i]);
+    }
+}
+
+/*
+vi:ts=4:nowrap:ai:expandtab:sw=4
+*/
