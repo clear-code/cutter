64bdfd2d81677dae3f5145cc07a07f815d0bf0f2
Kouhei Sutou <kou@clear-code.com>
2012-02-27 12:52:49 +0900

    win32: define fallbacks for wait(2) related macros
    I don't know about the fallback implementations are reasonable...

--- test/gcutter/test-gcut-process.c
+++ test/gcutter/test-gcut-process.c
@@ -1,4 +1,21 @@
 /* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ *  Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
@@ -7,11 +24,14 @@
 #include <string.h>
 #include <errno.h>
 #include <locale.h>
-#include <sys/types.h>
 #ifdef HAVE_SYS_WAIT_H
+#  include <sys/types.h>
 #  include <sys/wait.h>
 #else
-# define WEXITSTATUS(status) (status)
+#  define WIFEXITED(status) TRUE
+#  define WEXITSTATUS(status) (status)
+#  define WIFSIGNALED(status) TRUE
+#  define WTERMSIG(status) (status)
 #endif
 #include <signal.h>
 
