dbd41c24b5b98ee2c18ad10b58633641ebfdff60
HAYASHI Kentaro <hayashi@clear-code.com>
2012-08-07 19:34:48 +0900

    yum: add more gpg related definition

--- packages/yum/sign-rpm.sh
+++ packages/yum/sign-rpm.sh
@@ -21,5 +21,9 @@ run()
 
 for distribution in ${DISTRIBUTIONS}; do
     run rpm -D "_gpg_name ${GPG_UID}" \
+	-D "_gpg_digest_algo sha1" \
+	-D "__gpg /usr/bin/gpg2" \
+	-D "__gpg_check_password_cmd /bin/true true" \
+	-D "__gpg_sign_cmd %{__gpg} gpg --batch --no-verbose --no-armor %{?_gpg_digest_algo:--digest-algo %{_gpg_digest_algo}} --no-secmem-warning -u \"%{_gpg_name}\" -sbo %{__signature_filename} %{__plaintext_filename}" \
 	--resign $script_base_dir/${distribution}/*/*/*/*.rpm
 done
