c1fb8b656d1637ad65e28fde2c9a4df04b78f406
Kouhei Sutou <kou@clear-code.com>
2013-07-13 22:50:39 +0900

    Use GLib compatible API to suppress warnings
        cut-run-context.c: In function 'cut_run_context_init':
        cut-run-context.c:965:5: warning: implicit declaration of function 'g_mutex_new' [-Wimplicit-function-declaration]
             priv->mutex = g_mutex_new();
             ^
        cut-run-context.c:965:17: warning: assignment makes pointer from integer without a cast [enabled by default]
             priv->mutex = g_mutex_new();
                         ^
        cut-run-context.c: In function 'dispose':
        cut-run-context.c:1014:9: warning: implicit declaration of function 'g_mutex_free' [-Wimplicit-function-declaration]
                 g_mutex_free(priv->mutex);
                 ^

--- cutter/cut-run-context.c
+++ cutter/cut-run-context.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /*
- *  Copyright (C) 2008-2010  Kouhei Sutou <kou@clear-code.com>
+ *  Copyright (C) 2008-2013  Kouhei Sutou <kou@clear-code.com>
  *
  *  This library is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -32,6 +32,7 @@
 #include "cut-repository.h"
 #include "cut-test-case.h"
 #include "cut-test-result.h"
+#include "cut-glib-compatible.h"
 
 #include "cut-enum-types.h"
 #include <gcutter/gcut-marshalers.h>
