ed75f41979240135c7f57632a0cc32f20f2b3baf
Kenji Okimoto <okimoto@clear-code.com>
2013-11-07 12:01:16 +0900

    Revert "travis: follow download URL change on SF.net"
    This reverts commit bf343f9538d46ea2ac9d8ca673b2e60e97bf8eab.
    
    See https://github.com/clear-code/cutter/commit/bf343f9538d46ea2ac9d8ca673b2e60e97bf8eab#commitcomment-4520815

--- data/travis/setup.sh
+++ data/travis/setup.sh
@@ -5,7 +5,7 @@ set -e
 distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
 code_name=$(lsb_release --short --codename)
 component=main
-apt_url_base=http://downloads.sourceforge.net/cutter
+apt_url_base=http://downloads.sourceforge.net/project/cutter
 cat <<EOF | sudo tee /etc/apt/sources.list.d/cutter.list
 deb ${apt_url_base}/${distribution}/ ${code_name} ${component}
 deb-src ${apt_url_base}/${distribution}/ ${code_name} ${component}
