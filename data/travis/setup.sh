a24fe489e3d6dc9c4ecee1926435828461e9e4e4
Kouhei Sutou <kou@clear-code.com>
2014-06-21 14:37:13 +0900

    travis: use PPA for Ubuntu

--- data/travis/setup.sh
+++ data/travis/setup.sh
@@ -3,15 +3,23 @@
 set -e
 
 distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
-code_name=$(lsb_release --short --codename)
-component=main
-apt_url_base=http://downloads.sourceforge.net/project/cutter
-cat <<EOF | sudo tee /etc/apt/sources.list.d/cutter.list
+case $distribution in
+    debian)
+	code_name=$(lsb_release --short --codename)
+	component=main
+	apt_url_base=http://downloads.sourceforge.net/project/cutter
+	cat <<EOF | sudo tee /etc/apt/sources.list.d/cutter.list
 deb ${apt_url_base}/${distribution}/ ${code_name} ${component}
 deb-src ${apt_url_base}/${distribution}/ ${code_name} ${component}
 EOF
+	sudo apt-get update -qq
+	sudo apt-get install -qq -y --allow-unauthenticated cutter-keyring
+	;;
+    ubuntu)
+	sudo apt-get install -qq -y -V software-properties-common
+	sudo add-apt-repository -y ppa:cutter-testing-framework/ppa
+	;;
+esac
 
-sudo apt-get update -qq
-sudo apt-get install -qq -y --allow-unauthenticated cutter-keyring
 sudo apt-get update -qq
 sudo apt-get install -qq -y -V cutter-testing-framework
