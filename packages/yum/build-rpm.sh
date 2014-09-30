fe3e64ff9b2ca33c6b15e710dbc51375ee61e155
HAYASHI Kentaro <hayashi@clear-code.com>
2014-09-30 19:38:49 +0900

    rpm centos: use vagrant instead of chroot for building package

--- packages/yum/build-rpm.sh
+++ packages/yum/build-rpm.sh
@@ -1,127 +1,69 @@
 #!/bin/sh
 
-LANG=C
-
-PACKAGE=$(cat /tmp/build-package)
-VERSION=$(cat /tmp/build-version)
-SOURCE_BASE_NAME=$(cat /tmp/build-source-base-name)
-USER_NAME=$(cat /tmp/build-user)
-DEPENDED_PACKAGES=$(cat /tmp/depended-packages)
-USE_RPMFORGE=$(cat /tmp/build-use-rpmforge)
-USE_ATRPMS=$(cat /tmp/build-use-atrpms)
-BUILD_OPTIONS=$(cat /tmp/build-options)
-BUILD_SCRIPT=/tmp/build-${PACKAGE}.sh
-
 run()
 {
     "$@"
     if test $? -ne 0; then
-	echo "Failed $@"
-	exit 1
+        echo "Failed $@"
+        exit 1
     fi
 }
 
-yum_options=
+rpmbuild_options=
 
-distribution=$(cut -d ' ' -f 1 /etc/redhat-release | tr 'A-Z' 'a-z')
+. /vagrant/env.sh
+
+swap_file=/tmp/swap
+run dd if=/dev/zero of="$swap_file" bs=1024 count=3024K
+run mkswap "$swap_file"
+run swapon "$swap_file"
+
+distribution=$(cut -d " " -f 1 /etc/redhat-release | tr "A-Z" "a-z")
 if grep -q Linux /etc/redhat-release; then
-    distribution_version=$(cut -d ' ' -f 4 /etc/redhat-release)
+    distribution_version=$(cut -d " " -f 4 /etc/redhat-release)
 else
-    distribution_version=$(cut -d ' ' -f 3 /etc/redhat-release)
+    distribution_version=$(cut -d " " -f 3 /etc/redhat-release)
 fi
-if ! rpm -q ${distribution}-release > /dev/null 2>&1; then
-    packages_dir=/var/cache/yum/core/packages
-    release_rpm=${distribution}-release-${distribution_version}-*.rpm
-    run rpm -Uvh --force ${packages_dir}/${release_rpm}
-    run rpm -Uvh --force ${packages_dir}/ca-certificates-*.rpm
-fi
+distribution_version=$(echo ${distribution_version} | sed -e 's/\..*$//g')
 
-if test "$USE_RPMFORGE" = "yes"; then
-    if ! rpm -q rpmforge-release > /dev/null 2>&1; then
-	architecture=$(cut -d '-' -f 1 /etc/rpm/platform)
-	rpmforge_url=http://packages.sw.be/rpmforge-release
-	rpmforge_rpm_base=rpmforge-release-0.5.2-2.el5.rf.${architecture}.rpm
-	wget $rpmforge_url/$rpmforge_rpm_base
-	run rpm -Uvh $rpmforge_rpm_base
-	rm $rpmforge_rpm_base
-	sed -i'' -e 's/enabled = 1/enabled = 0/g' /etc/yum.repos.d/rpmforge.repo
-    fi
-    yum_options="$yum_options --enablerepo=rpmforge"
-fi
+architecture="$(arch)"
+case "${architecture}" in
+    i*86)
+        architecture=i386
+        ;;
+esac
 
-if test "$USE_ATRPMS" = "yes"; then
-    case "$(cat /etc/redhat-release)" in
-	CentOS*)
-	    repository_label=CentOS
-	    repository_prefix=el
-	    ;;
-	*)
-	    repository_label=Fedora
-	    repository_prefix=f
-	    ;;
-    esac
-    cat <<EOF > /etc/yum.repos.d/atrpms.repo
-[atrpms]
-name=${repository_label} \$releasever - \$basearch - ATrpms
-baseurl=http://dl.atrpms.net/${repository_prefix}\$releasever-\$basearch/atrpms/stable
-gpgkey=http://ATrpms.net/RPM-GPG-KEY.atrpms
-gpgcheck=1
-enabled=0
-EOF
-    yum_options="$yum_options --enablerepo=atrpms"
-fi
-
-rpmbuild_options="${BUILD_OPTIONS}"
-
-run yum update ${yum_options} -y
-run yum install ${yum_options} -y rpm-build rpmdevtools tar ${DEPENDED_PACKAGES}
-run yum clean ${yum_options} packages
-
-if ! id $USER_NAME >/dev/null 2>&1; then
-    run useradd -m $USER_NAME
-fi
-
-cat <<EOF > $BUILD_SCRIPT
-#!/bin/sh
+run yum groupinstall -y "Development Tools"
+run yum install -y rpm-build rpmdevtools tar ${DEPENDED_PACKAGES}
 
 if [ -x /usr/bin/rpmdev-setuptree ]; then
     rm -rf .rpmmacros
-    rpmdev-setuptree
+    run rpmdev-setuptree
 else
-    cat <<EOM > ~/.rpmmacros
-%_topdir \$HOME/rpmbuild
+    run cat <<EOM > ~/.rpmmacros
+%_topdir ${HOME}/rpmbuild
 EOM
-
-    mkdir -p rpmbuild/SOURCES
-    mkdir -p rpmbuild/SPECS
-    mkdir -p rpmbuild/BUILD
-    mkdir -p rpmbuild/RPMS
-    mkdir -p rpmbuild/SRPMS
+    run mkdir -p ~/rpmbuild/SOURCES
+    run mkdir -p ~/rpmbuild/SPECS
+    run mkdir -p ~/rpmbuild/BUILD
+    run mkdir -p ~/rpmbuild/RPMS
+    run mkdir -p ~/rpmbuild/SRPMS
 fi
 
-if test -f /tmp/${SOURCE_BASE_NAME}-$VERSION-*.src.rpm; then
-    if ! rpm -Uvh /tmp/${SOURCE_BASE_NAME}-$VERSION-*.src.rpm; then
-        cd rpmbuild/SOURCES
-        rpm2cpio /tmp/${SOURCE_BASE_NAME}-$VERSION-*.src.rpm | cpio -id
-        if ! yum info tcp_wrappers-devel >/dev/null 2>&1; then
-            sed -i'' -e 's/tcp_wrappers-devel/tcp_wrappers/g' ${PACKAGE}.spec
-        fi
-        if ! yum info libdb-devel >/dev/null 2>&1; then
-            sed -i'' -e 's/libdb-devel/db4-devel/g' ${PACKAGE}.spec
-        fi
-        sed -i'' -e 's/BuildArch: noarch//g' ${PACKAGE}.spec
-        mv ${PACKAGE}.spec ../SPECS/
-        cd
-    fi
-else
-    cp /tmp/${SOURCE_BASE_NAME}-$VERSION.* rpmbuild/SOURCES/
-    cp /tmp/${PACKAGE}.spec rpmbuild/SPECS/
-fi
+repository="/vagrant/repositories/${distribution}/${distribution_version}"
+rpm_dir="${repository}/${architecture}/Packages"
+srpm_dir="${repository}/source/SRPMS"
+run mkdir -p "${rpm_dir}" "${srpm_dir}"
 
-chmod o+rx . rpmbuild rpmbuild/RPMS rpmbuild/SRPMS
+# for debug
+# rpmbuild_options="$rpmbuild_options --define 'optflags -O0 -g3'"
 
-rpmbuild -ba ${rpmbuild_options} rpmbuild/SPECS/${PACKAGE}.spec
-EOF
+cd
 
-run chmod +x $BUILD_SCRIPT
-run su - $USER_NAME $BUILD_SCRIPT
+run cp /vagrant/tmp/${PACKAGE}-${VERSION}.* rpmbuild/SOURCES/
+run cp /vagrant/tmp/${distribution}/${PACKAGE}.spec rpmbuild/SPECS/
+
+run rpmbuild -ba ${rpmbuild_options} rpmbuild/SPECS/${PACKAGE}.spec
+
+run mv rpmbuild/RPMS/*/* "${rpm_dir}/"
+run mv rpmbuild/SRPMS/* "${srpm_dir}/"
