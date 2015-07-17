95dd18ec7eade1e7e002aabd539a9cff143e8f78
HAYASHI Kentaro <hayashi@clear-code.com>
2015-07-17 11:15:07 +0900

    packages apt: use vagrant to build packages

--- packages/apt/build-deb.sh
+++ packages/apt/build-deb.sh
@@ -2,71 +2,56 @@
 
 LANG=C
 
-PACKAGE=$(cat /tmp/build-package)
-USER_NAME=$(cat /tmp/build-user)
-VERSION=$(cat /tmp/build-version)
-DEPENDED_PACKAGES=$(cat /tmp/depended-packages)
-BUILD_SCRIPT=/tmp/build-deb-in-chroot.sh
-CODE_NAME=$(lsb_release -s -c)
-GSTREAMER_INSTALL=gstreamer0.10-plugins-cutter.install
-
 run()
 {
-    "$@"
-    if test $? -ne 0; then
-	echo "Failed $@"
-	exit 1
-    fi
+  "$@"
+  if test $? -ne 0; then
+    echo "Failed $@"
+    exit 1
+  fi
 }
 
-if [ ! -x /usr/bin/aptitude ]; then
-    run apt-get update
-    run apt-get install -y aptitude
-fi
-run aptitude update -V -D
-run aptitude safe-upgrade -V -D -y
+. /vagrant/tmp/env.sh
 
-if ! aptitude show libgoffice-0.8-dev > /dev/null 2>&1; then
-    DEPENDED_PACKAGES=$(echo $DEPENDED_PACKAGES | sed -e 's/libgoffice-0.8-dev//')
-fi
+run apt-get update
+run apt-get install -y lsb-release
 
-run aptitude install -V -D -y devscripts ${DEPENDED_PACKAGES}
-run aptitude clean
-
-if ! id $USER_NAME >/dev/null 2>&1; then
-    run useradd -m $USER_NAME
-fi
-
-if test "${PACKAGE}" = "cutter"; then
-    ARCHITECTURE=$(dpkg --print-architecture)
-    if test ${ARCHITECTURE} = "amd64"; then
-        ARCHITECTURE="x86_64";
-    fi
-    if [ -d "/usr/lib/${ARCHITECTURE}-linux-gnu/gstreamer-0.10" ]; then
-        LIB_ARCHITECTURE="${ARCHITECTURE}-linux-gnu"
-        sed -i'' -e "s/usr\/lib/usr\/lib\/${LIB_ARCHITECTURE}/" \
-            /tmp/${PACKAGE}-debian/${GSTREAMER_INSTALL}
-    fi
-fi
-
-cat <<EOF > $BUILD_SCRIPT
-#!/bin/sh
-
-rm -rf build
-mkdir -p build
-
-cp /tmp/${PACKAGE}-${VERSION}.tar.gz build/${PACKAGE}_${VERSION}.orig.tar.gz
-cd build
-tar xfz ${PACKAGE}_${VERSION}.orig.tar.gz
-cd ${PACKAGE}-${VERSION}/
-cp -rp /tmp/${PACKAGE}-debian debian
-if ! dpkg -l libgoffice-0.8-dev > /dev/null 2>&1; then
-    mv debian/control debian/control.tmp
-    grep -v libgoffice-0.8-dev debian/control.tmp > debian/control
-    rm debian/control.tmp
-fi
-debuild -us -uc
+distribution=$(lsb_release --id --short | tr 'A-Z' 'a-z')
+code_name=$(lsb_release --codename --short)
+case "${distribution}" in
+  debian)
+    component=main
+    run cat <<EOF > /etc/apt/sources.list.d/cutter.list
+deb http://sourceforge.net/projects/cutter/files/debian/ ${code_name} main
+deb-src http://sourceforge.net/projects/cutter/files/debian/ ${code_name} main
 EOF
+    run apt-get update
+    run apt-get install -y --allow-unauthenticated cutter-keyring
+    run apt-get update
+    ;;
+  ubuntu)
+    component=universe
+    run apt-get -y install software-properties-common
+    run add-apt-repository -y main
+    run add-apt-repository -y ppa:cutter-testing-framework/ppa
+    run apt-get update
+    ;;
+esac
 
-run chmod +x $BUILD_SCRIPT
-run su - $USER_NAME $BUILD_SCRIPT
+run apt-get install -V -y build-essential devscripts ${DEPENDED_PACKAGES}
+
+run mkdir -p build
+run cp /vagrant/tmp/${PACKAGE}-${VERSION}.tar.gz \
+  build/${PACKAGE}_${VERSION}.orig.tar.gz
+run cd build
+run tar xfz ${PACKAGE}_${VERSION}.orig.tar.gz
+run cd ${PACKAGE}-${VERSION}/
+run cp -rp /vagrant/tmp/debian debian
+# export DEB_BUILD_OPTIONS=noopt
+run debuild -us -uc
+run cd -
+
+package_initial=$(echo "${PACKAGE}" | sed -e 's/\(.\).*/\1/')
+pool_dir="/vagrant/repositories/${distribution}/pool/${code_name}/${component}/${package_initial}/${PACKAGE}"
+run mkdir -p "${pool_dir}/"
+run cp *.tar.* *.dsc *.deb "${pool_dir}/"
