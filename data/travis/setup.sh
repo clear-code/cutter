2e9408379a86aebbe673430b6335b6ae55f74374
Kenji Okimoto <okimoto@clear-code.com>
2017-06-21 16:57:28 +0900

    Indent

--- data/travis/setup.sh
+++ data/travis/setup.sh
@@ -18,28 +18,28 @@ if [ "$CUTTER_MASTER" = "yes" ]; then
     sudo make install > /dev/null
     cd ..
 else
-distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
-case $distribution in
-    debian)
-	code_name=$(lsb_release --short --codename)
-        if [ "x${CODE_NAME}" != "x" ]; then
-            code_name=$CODE_NAME
-        fi
-	component=main
-	apt_url_base=https://sourceforge.net/projects/cutter/files
-	cat <<EOF | sudo tee /etc/apt/sources.list.d/cutter.list
+    distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
+    case $distribution in
+        debian)
+	    code_name=$(lsb_release --short --codename)
+            if [ "x${CODE_NAME}" != "x" ]; then
+                code_name=$CODE_NAME
+            fi
+	    component=main
+	    apt_url_base=https://sourceforge.net/projects/cutter/files
+	    cat <<EOF | sudo tee /etc/apt/sources.list.d/cutter.list
 deb ${apt_url_base}/${distribution}/ ${code_name} ${component}
 deb-src ${apt_url_base}/${distribution}/ ${code_name} ${component}
 EOF
-	sudo apt-get update -qq
-	sudo apt-get install -qq -y --allow-unauthenticated cutter-keyring
-	;;
-    ubuntu)
-	sudo apt-get install -qq -y -V software-properties-common
-	sudo add-apt-repository -y ppa:cutter-testing-framework/ppa
-	;;
-esac
+	    sudo apt-get update -qq
+	    sudo apt-get install -qq -y --allow-unauthenticated cutter-keyring
+	    ;;
+        ubuntu)
+	    sudo apt-get install -qq -y -V software-properties-common
+	    sudo add-apt-repository -y ppa:cutter-testing-framework/ppa
+	    ;;
+    esac
 
-sudo apt-get update -qq
-sudo apt-get install -qq -y -V cutter-testing-framework
+    sudo apt-get update -qq
+    sudo apt-get install -qq -y -V cutter-testing-framework
 fi
