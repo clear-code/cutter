730d3d815e3337690e4a225dfcd3d78025f4e7ad
Kouhei Sutou <kou@clear-code.com>
2019-04-14 05:49:29 +0900

    Use 2-spaces for indent in .sh

--- autogen.sh
+++ autogen.sh
@@ -4,33 +4,33 @@ set -u
 
 run()
 {
-    "$@"
-    if test $? -ne 0; then
-	echo "Failed $@"
-	exit 1
-    fi
+  "$@"
+  if test $? -ne 0; then
+    echo "Failed $@"
+    exit 1
+  fi
 }
 
 # for old intltoolize
 if [ ! -e config/po ]; then
-    ln -s ../po config/po
+  ln -s ../po config/po
 fi
 
-case `uname -s` in
-    Darwin)
-        homebrew_aclocal=/usr/local/share/aclocal
-        if [ -d $homebrew_aclocal ]; then
-	  ACLOCAL_OPTIONS="$ACLOCAL_OPTIONS -I $homebrew_aclocal"
-        fi
-        gettext_aclocal="$(echo /usr/local/Cellar/gettext/*/share/aclocal)"
-        if [ -d $gettext_aclocal ]; then
-	  ACLOCAL_OPTIONS="$ACLOCAL_OPTIONS -I $gettext_aclocal"
-        fi
-	: ${LIBTOOLIZE=glibtoolize}
-	;;
-    FreeBSD)
-	ACLOCAL_OPTIONS="$ACLOCAL_OPTIONS -I /usr/local/share/aclocal/"
-	;;
+case $(uname -s) in
+  Darwin)
+    homebrew_aclocal=/usr/local/share/aclocal
+    if [ -d $homebrew_aclocal ]; then
+      ACLOCAL_OPTIONS="$ACLOCAL_OPTIONS -I $homebrew_aclocal"
+    fi
+    gettext_aclocal="$(echo /usr/local/Cellar/gettext/*/share/aclocal)"
+    if [ -d $gettext_aclocal ]; then
+      ACLOCAL_OPTIONS="$ACLOCAL_OPTIONS -I $gettext_aclocal"
+    fi
+    : ${LIBTOOLIZE=glibtoolize}
+    ;;
+  FreeBSD)
+    ACLOCAL_OPTIONS="$ACLOCAL_OPTIONS -I /usr/local/share/aclocal/"
+    ;;
 esac
 
 run ${INTLTOOLIZE:-intltoolize} --force --copy
