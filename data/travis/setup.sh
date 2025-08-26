761886b6d6aa3742379883a21b6a3c5982ae28c4
Sutou Kouhei <kou@clear-code.com>
2025-08-26 15:29:48 +0900

    data/travis/setup.sh: add support for AlmaLinux 8-10

--- data/travis/setup.sh
+++ data/travis/setup.sh
@@ -3,18 +3,48 @@
 set -e
 
 if [ "$CUTTER_MASTER" = "yes" ] || [ "$CUTTER_MAIN" = "yes" ] ; then
-    sudo apt update -qq
-    sudo apt install -qq -y \
-         autotools-dev \
-         intltool \
-         libsoup2.4-dev
-    git clone --depth 1 https://github.com/clear-code/cutter.git
-    cd cutter
-    ./autogen.sh
-    ./configure --prefix=/usr --localstatedir=/var --enable-debug
-    make -j > /dev/null
-    sudo make install > /dev/null
-    cd ..
+    if type apt > /dev/null 2>&1; then
+        sudo apt update -qq
+        sudo apt install -qq -y \
+             autotools-dev \
+             intltool \
+             libsoup2.4-dev
+        git clone --depth 1 https://github.com/clear-code/cutter.git
+        cd cutter
+        ./autogen.sh
+        ./configure --prefix=/usr --localstatedir=/var --enable-debug
+        make -j > /dev/null
+        sudo make install > /dev/null
+        cd ..
+    else
+        case $(. /etc/os-release && echo "${VERSION_ID}") in
+            8.*)
+                additional_repository=powertools
+                ;;
+            *)
+                additional_repository=crb
+                ;;
+        esac
+        sudo dnf install --enablerepo=${additional_repository} -y \
+             autoconf \
+             automake \
+             diffutils \
+             gcc \
+             gcc-c++ \
+             git \
+             glib2-devel \
+             gtk-doc \
+             intltool \
+             libtool \
+             make
+        git clone --depth 1 https://github.com/clear-code/cutter.git
+        cd cutter
+        ./autogen.sh
+        ./configure --prefix=/usr --localstatedir=/var --enable-debug
+        make -j > /dev/null
+        sudo make install > /dev/null
+        cd ..
+    fi
 else
     distribution=$(lsb_release --short --id | tr 'A-Z' 'a-z')
     case $distribution in
